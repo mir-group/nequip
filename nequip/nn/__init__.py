fd0011df441debb4dd6dfaab2827861766617d25
Alby M <1473644+Linux-cpp-lisp@users.noreply.github.com>
2021-03-21 18:51:55 -0600

    Add ConvNetLayer

--- nequip/nn/__init__.py
+++ nequip/nn/__init__.py
@@ -4,3 +4,4 @@ from ._interaction_block import InteractionBlock  # noqa: F401
 from ._grad_output import GradientOutput  # noqa: F401
 from ._rescale import RescaleOutput  # noqa: F401
 from ._convnet import ConvNet  # noqa: F401
+from ._convnetlayer import ConvNetLayer  # noqa: F401
