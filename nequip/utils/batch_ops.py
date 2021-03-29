e944c3d26e829b0bbcf67576990ee38980fa8df0
Alby M <1473644+Linux-cpp-lisp@users.noreply.github.com>
2021-03-29 10:45:42 -0600

    Batched `bincount`

---
+++ nequip/utils/batch_ops.py
@@ -0,0 +1,34 @@
+from typing import Optional
+
+import torch
+
+from torch_scatter import scatter
+
+
+def bincount(
+    input: torch.Tensor, batch: Optional[torch.Tensor] = None, minlength: int = 0
+):
+    assert input.ndim == 1
+    assert batch.shape == input.shape
+    if batch is None:
+        return torch.bincount(input, minlength=minlength)
+
+    length: int = input.max().item() + 1
+    if minlength == 0:
+        minlength = length
+    if length > minlength:
+        raise ValueError(
+            f"minlength {minlength} too small for input with integers up to and including {length}"
+        )
+
+    # Flatten indexes
+    # Make each "class" in input into a per-input class.
+    input = input + batch * minlength
+
+    num_batch = batch.max() + 1
+
+    return scatter(
+        torch.ones(1, device=input.device, dtype=input.dtype).expand(input.shape),
+        input,
+        dim_size=minlength * num_batch,
+    ).reshape(num_batch, minlength)
