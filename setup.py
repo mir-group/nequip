3a7d948ee29121f01344ee63a4c74361f5996fee
Alby M <1473644+Linux-cpp-lisp@users.noreply.github.com>
2021-04-06 10:26:34 -0600

    Entry points

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup
+from pathlib import Path
 
 # see https://packaging.python.org/guides/single-sourcing-package-version/
 version_dict = {}
-with open("...nequip/_version.py") as fp:
+with open(Path(__file__).parents[0] / "nequip/_version.py") as fp:
     exec(fp.read(), version_dict)
 version = version_dict["__version__"]
 del version_dict
@@ -15,6 +16,15 @@ setup(
     author="Simon Batzner, Anders Johansson, Albert Musealian, Lixin Sun, Mario Geiger, Tess Smidt",
     python_requires=">=3.8",
     packages=["nequip"],
+    entry_points={
+        # make the scripts available as command line scripts
+        "console_scripts": [
+            "nequip_train = nequip._scripts.train:main",
+            "nequip_restart = nequip._scripts.restart:main",
+            "nequip_requeue = nequip._scripts.requeue:main",
+            "nequip_deploy = nequip._scripts.deploy:main",
+        ]
+    },
     install_requires=[
         "numpy",
         "scipy",
