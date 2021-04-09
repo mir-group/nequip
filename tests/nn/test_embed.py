d87c222203aa5a2175e82def4557f5359b5d98a9
Alby M <1473644+Linux-cpp-lisp@users.noreply.github.com>
2021-04-09 12:03:12 -0600

    Some extra equivariance tests

--- tests/nn/test_embed.py
+++ tests/nn/test_embed.py
@@ -1,5 +1,9 @@
+import torch
+
 from e3nn.util.test import assert_auto_jitable
 
+from nequip.data import AtomicDataDict
+from nequip.utils.test import assert_AtomicData_equivariant
 from nequip.nn.radial_basis import BesselBasis
 from nequip.nn.cutoffs import PolynomialCutoff
 from nequip.nn.embedding import (
@@ -9,22 +13,35 @@ from nequip.nn.embedding import (
 )
 
 
-def test_onehot_jit():
-    oh = OneHotAtomEncoding(allowed_species=[1, 4, 5])
+def test_onehot(CH3CHO):
+    _, data = CH3CHO
+    oh = OneHotAtomEncoding(
+        allowed_species=torch.unique(data[AtomicDataDict.ATOMIC_NUMBERS_KEY]),
+        irreps_in=data.irreps,
+    )
     assert_auto_jitable(oh)
+    assert_AtomicData_equivariant(oh, data)
 
 
-def test_spharm_jit():
+def test_spharm(CH3CHO):
+    _, data = CH3CHO
     sph = SphericalHarmonicEdgeAttrs(
-        irreps_edge_sh=3, irreps_in={"another_field": "2x0e"}
+        irreps_edge_sh="0e + 1o + 2e", irreps_in=data.irreps
     )
     assert_auto_jitable(sph)
+    assert_AtomicData_equivariant(sph, data)
 
 
-def test_radial_basis_jit():
+def test_radial_basis(CH3CHO):
+    _, data = CH3CHO
     basis = BesselBasis
     cutoff = PolynomialCutoff
     rad = RadialBasisEdgeEncoding(
-        basis, cutoff, basis_kwargs={"r_max": 5.0}, cutoff_kwargs={"r_max": 5.0}
+        basis,
+        cutoff,
+        basis_kwargs={"r_max": 5.0},
+        cutoff_kwargs={"r_max": 5.0},
+        irreps_in=data.irreps,
     )
     assert_auto_jitable(rad)
+    assert_AtomicData_equivariant(rad, data)
