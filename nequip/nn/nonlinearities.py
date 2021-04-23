62a5efa1da32a23984ea51b88fd28eb3c94e38c3
Alby M <1473644+Linux-cpp-lisp@users.noreply.github.com>
2021-04-23 11:42:32 -0600

    buffer -> float

--- nequip/nn/nonlinearities.py
+++ nequip/nn/nonlinearities.py
@@ -1,5 +1,7 @@
 import torch
 
+import math
+
 
 class _ShiftedSoftPlus(torch.nn.Module):
     """
@@ -9,10 +11,12 @@ class _ShiftedSoftPlus(torch.nn.Module):
     :param threshold: values above are linear function, default = 20
     """
 
+    _log2: float
+
     def __init__(self, beta=1, threshold=20):
         super().__init__()
         self.softplus = torch.nn.Softplus(beta=beta, threshold=threshold)
-        self.register_buffer("log2", torch.log(torch.as_tensor(2.0)))
+        self._log2 = math.log(2.0)
 
     def forward(self, x):
         """
@@ -21,7 +25,7 @@ class _ShiftedSoftPlus(torch.nn.Module):
         :param x: torch.Tensor, input
         :return: torch.Tensor, ssp(x)
         """
-        return self.softplus(x) - self.log2
+        return self.softplus(x) - self._log2
 
 
 ShiftedSoftPlus = _ShiftedSoftPlus()
