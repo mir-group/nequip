6dc5229ac29b13f3ff7866b87ae6211492a70765
nw13slx <nw13mifaso@gmail.com>
2021-05-13 15:52:04 -0400

    update full.yaml

--- nequip/train/early_stopping.py
+++ nequip/train/early_stopping.py
@@ -11,17 +11,17 @@ class EarlyStopping:
 
     1. a value lower than a defined lower bound
     2. a value higher than a defined upper bound
-    3. a value hasn't decreased for x epochs within min_delta range
+    3. a value hasn't decreased for x epochs within delta range
 
     Args:
 
     lower_bounds (dict): define the key and lower bound for condition 1
     upper_bounds (dict): define the key and lower bound for condition 2
     patiences (dict): defined the x epochs for condition 3
-    min_delta (dict): defined the delta range for condition 3. defaults are 0.0
+    delta (dict): defined the delta range for condition 3. defaults are 0.0
     cumulative_delta (bool): if True, the minimum value recorded for condition 3
                              will not be updated when the newer value only decreases
-                             for a tiny value (< min_delta). default False
+                             for a tiny value (< delta). default False
     """
 
     def __init__(
@@ -29,7 +29,7 @@ class EarlyStopping:
         lower_bounds: dict = {},
         upper_bounds: dict = {},
         patiences: dict = {},
-        min_delta: dict = {},
+        delta: dict = {},
         cumulative_delta: bool = False,
     ):
 
@@ -40,23 +40,23 @@ class EarlyStopping:
 
         # self.keys = set(list(self.lower_bounds.keys())) + set(list(self.upper_bounds.keys()))+set(list(self.patiences.keys()))
 
-        self.min_delta = {}
+        self.delta = {}
         self.counters = {}
         self.minimums = {}
         for key, pat in self.patiences.items():
             self.patiences[key] = int(pat)
             self.counters[key] = 0
             self.minimums[key] = None
-            self.min_delta[key] = min_delta.get(key, 0.0)
+            self.delta[key] = delta.get(key, 0.0)
 
             if pat < 1:
                 raise ValueError(
                     f"Argument patience for {key} should be positive integer."
                 )
-            if self.min_delta[key] < 0.0:
-                raise ValueError("Argument min_delta should not be a negative number.")
+            if self.delta[key] < 0.0:
+                raise ValueError("Argument delta should not be a negative number.")
 
-        for key in self.min_delta:
+        for key in self.delta:
             if key not in self.patiences:
                 raise ValueError(f"patience for {key} should be defined")
 
@@ -71,11 +71,11 @@ class EarlyStopping:
 
             value = metrics[key]
             minimum = self.minimums[key]
-            min_delta = self.min_delta[key]
+            delta = self.delta[key]
 
             if minimum is None:
                 self.minimums[key] = value
-            elif value >= (minimum - min_delta):
+            elif value >= (minimum - delta):
                 if not self.cumulative_delta and value > minimum:
                     self.minimums[key] = value
                 self.counters[key] += 1
