46a966b8a592b284dfca0b5ae18a4d19b0909260
Linux-cpp-lisp <1473644+Linux-cpp-lisp@users.noreply.github.com>
2021-08-05 17:16:06 -0400

    test error using metrics

--- nequip/scripts/test_error.py
+++ nequip/scripts/test_error.py
@@ -1,15 +1,17 @@
 import sys
 import argparse
 from pathlib import Path
+from numpy import disp
+from tqdm.auto import tqdm
 
 import torch
 
 from nequip.utils import Config, dataset_from_config
-from nequip.data import AtomicDataDict, AtomicData, Collater
+from nequip.data import AtomicData, Collater
 from nequip.scripts.deploy import load_deployed_model
-from nequip.utils import load_file
-
-from torch_runstats import RunningStats
+from nequip.utils import load_file, instantiate
+from nequip.train.loss import Loss
+from nequip.train.metrics import Metrics
 
 
 def main(args=None):
@@ -34,6 +36,12 @@ def main(args=None):
         default=None,
     )
     parser.add_argument(
+        "--metrics-config",
+        help="A YAML config file specifying the metrics to compute. If omitted, `config_final.yaml` in `train_dir` will be used. If the config does not specify `metrics_components`, the default is to print MAEs and RMSEs for all fields given in the loss function.",
+        type=Path,
+        default=None,
+    )
+    parser.add_argument(
         "--test-indexes",
         help="Path to a file containing the indexes in the dataset that make up the test set. If omitted, all data frames *not* used as training or validation data in the training session `train_dir` will be used.",
         type=Path,
@@ -45,18 +53,14 @@ def main(args=None):
         type=int,
         default=5,
     )
-    parser.add_argument(
-        "--log-every",
-        help="Log approximately every n datapoints.",
-        type=int,
-        default=10,
-    )
     args = parser.parse_args(args=args)
 
     # Do the defaults:
     if args.train_dir:
         if args.dataset_config is None:
             args.dataset_config = args.train_dir / "config_final.yaml"
+        if args.metrics_config is None:
+            args.metrics_config = args.train_dir / "config_final.yaml"
         if args.model is None:
             args.model = args.train_dir / "best_model.pth"
         if args.test_indexes is None:
@@ -71,18 +75,25 @@ def main(args=None):
     # validate
     if args.dataset_config is None:
         raise ValueError("--dataset-config or --train-dir must be provided")
+    if args.metrics_config is None:
+        raise ValueError("--metrics-config or --train-dir must be provided")
     if args.model is None:
         raise ValueError("--model or --train-dir must be provided")
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Using device: {device}", file=sys.stderr)
 
     # Load model:
+    print("Loading model... ", file=sys.stderr, end="")
     try:
         model, _ = load_deployed_model(args.model, device=device)
+        print("loaded deployed model.", file=sys.stderr)
     except ValueError:  # its not a deployed model
         model = torch.load(args.model, map_location=device)
+        print("loaded pickled Python model.", file=sys.stderr)
 
     # Load a config file
+    print("Loading dataset...", file=sys.stderr)
     config = Config.from_file(str(args.dataset_config))
     dataset = dataset_from_config(config)
     c = Collater.for_dataset(dataset, exclude_keys=[])
@@ -99,47 +110,74 @@ def main(args=None):
         # load from file
         test_idcs = load_file(args.test_indexes)
 
-    # Do the stats
-    e_stats = RunningStats()
-    e_stats.to(device=device, dtype=torch.get_default_dtype())
-    f_stats = RunningStats(dim=(3,), reduce_dims=(0,))
-    f_stats.to(device=device, dtype=torch.get_default_dtype())
+    # Figure out what metrics we're actually computing
+    metrics_config = Config.from_file(str(args.metrics_config))
+    metrics_components = metrics_config.get("metrics_components", None)
+    # See trainer.py: init() and init_metrics()
+    # Default to loss functions if no metrics specified:
+    if metrics_components is None:
+        loss, _ = instantiate(
+            builder=Loss,
+            prefix="loss",
+            positional_args=dict(coeffs=metrics_config.loss_coeffs),
+            all_args=metrics_config,
+        )
+        metrics_components = []
+        for key, func in loss.funcs.items():
+            params = {
+                "PerSpecies": type(func).__name__.startswith("PerSpecies"),
+            }
+            metrics_components.append((key, "mae", params))
+            metrics_components.append((key, "rmse", params))
+
+    metrics, _ = instantiate(
+        builder=Metrics,
+        prefix="metrics",
+        positional_args=dict(components=metrics_components),
+        all_args=metrics_config,
+    )
+    metrics.to(device=device)
 
     batch_i: int = 0
     batch_size: int = args.batch_size
-    since_last_log: int = 0
 
-    while True:
-        datas = [
-            dataset.get(int(idex))
-            for idex in test_idcs[batch_i * batch_size : (batch_i + 1) * batch_size]
-        ]
-        since_last_log += len(datas)
-        if len(datas) == 0:
-            break
-        batch = c.collate(datas)
-        batch = batch.to(device)
-        out = model(AtomicData.to_AtomicDataDict(batch))
-        e = out[AtomicDataDict.TOTAL_ENERGY_KEY].detach()
-        f = out[AtomicDataDict.FORCE_KEY].detach()
-        e_stats.accumulate_batch((e - batch[AtomicDataDict.TOTAL_ENERGY_KEY]).abs())
-        f_stats.accumulate_batch((f - batch[AtomicDataDict.FORCE_KEY]).abs())
+    print("Starting...", file=sys.stderr)
+    with tqdm(bar_format="{desc}") as display_bar:
+        with tqdm(total=len(test_idcs)) as prog:
+            while True:
+                datas = [
+                    dataset.get(int(idex))
+                    for idex in test_idcs[
+                        batch_i * batch_size : (batch_i + 1) * batch_size
+                    ]
+                ]
+                if len(datas) == 0:
+                    break
+                batch = c.collate(datas)
+                batch = batch.to(device)
+                out = model(AtomicData.to_AtomicDataDict(batch))
+                # Accumulate metrics
+                with torch.no_grad():
+                    metrics(out, batch)
 
-        if since_last_log >= args.log_every:
-            print(
-                "Progress: {:.2f}%, cumulative MAE-F: {}, cumulative MAE-E: {}".format(
-                    (e_stats.n.cpu().item() * 100) / len(test_idcs),
-                    e_stats.current_result().cpu().item(),
-                    f_stats.current_result().cpu().item(),
-                ),
-                file=sys.stderr,
-            )
-            since_last_log = 0
+                batch_i += 1
+                display_bar.set_description_str(
+                    " | ".join(
+                        f"{k[0]}_{k[1]} = {v.cpu().item(): 4.2f}"
+                        for k, v in metrics.current_result().items()
+                    )
+                )
+                prog.update(batch.num_graphs)
+            display_bar.close()
+        prog.close()
 
-        batch_i += 1
-
-    print("Force MAE: {}".format(f_stats.current_result().cpu().item()))
-    print("Energy MAE: {}".format(e_stats.current_result().cpu().item()))
+    print("--- Final result: ---")
+    print(
+        "\n".join(
+            f"{k[0]}_{k[1]} = {v.cpu().item():f}"
+            for k, v in metrics.current_result().items()
+        )
+    )
 
 
 if __name__ == "__main__":
