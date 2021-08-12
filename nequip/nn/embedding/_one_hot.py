2f468154b4fc9cc81ac6ab716d00dc19f6eeadd7
Linux-cpp-lisp <1473644+Linux-cpp-lisp@users.noreply.github.com>
2021-08-12 15:35:13 -0400

    Remove atomic numbers from model

--- nequip/nn/embedding/_one_hot.py
+++ nequip/nn/embedding/_one_hot.py
@@ -18,25 +18,11 @@ class OneHotAtomEncoding(GraphModuleMixin, torch.nn.Module):
     # Docstrings
     def __init__(
         self,
-        allowed_species=None,
-        num_species: int = None,
+        num_species: int,
         set_features: bool = True,
         irreps_in=None,
     ):
         super().__init__()
-        if allowed_species is not None and num_species is not None:
-            raise ValueError("allowed_species and num_species cannot both be provided.")
-
-        if allowed_species is not None:
-            num_species = len(allowed_species)
-            allowed_species = torch.as_tensor(allowed_species)
-            self.register_buffer("_min_Z", allowed_species.min())
-            self.register_buffer("_max_Z", allowed_species.max())
-            Z_to_index = torch.full(
-                (1 + self._max_Z - self._min_Z,), -1, dtype=torch.long
-            )
-            Z_to_index[allowed_species - self._min_Z] = torch.arange(num_species)
-            self.register_buffer("_Z_to_index", Z_to_index)
         self.num_species = num_species
         self.set_features = set_features
         # Output irreps are num_species even (invariant) scalars
@@ -49,27 +35,8 @@ class OneHotAtomEncoding(GraphModuleMixin, torch.nn.Module):
             ]
         self._init_irreps(irreps_in=irreps_in, irreps_out=irreps_out)
 
-    @torch.jit.export
-    def index_for_atomic_numbers(self, atomic_nums: torch.Tensor):
-        if atomic_nums.min() < self._min_Z or atomic_nums.max() > self._max_Z:
-            raise RuntimeError("Invalid atomic numbers for this OneHotEncoding")
-
-        out = self._Z_to_index[atomic_nums - self._min_Z]
-        assert out.min() >= 0, "Invalid atomic numbers for this OneHotEncoding"
-        return out
-
     def forward(self, data: AtomicDataDict.Type):
-        if AtomicDataDict.SPECIES_INDEX_KEY in data:
-            type_numbers = data[AtomicDataDict.SPECIES_INDEX_KEY]
-        elif AtomicDataDict.ATOMIC_NUMBERS_KEY in data:
-            type_numbers = self.index_for_atomic_numbers(
-                data[AtomicDataDict.ATOMIC_NUMBERS_KEY]
-            )
-            data[AtomicDataDict.SPECIES_INDEX_KEY] = type_numbers
-        else:
-            raise ValueError(
-                "Nothing in this `data` to encode, need either species index or atomic numbers"
-            )
+        type_numbers = data[AtomicDataDict.SPECIES_INDEX_KEY]
         one_hot = torch.nn.functional.one_hot(
             type_numbers, num_classes=self.num_species
         ).to(device=type_numbers.device, dtype=data[AtomicDataDict.POSITIONS_KEY].dtype)
