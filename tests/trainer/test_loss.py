fa0d7bd08bcccf1936bc623b45157b4e48d4b4de
nw13slx <nw13mifaso@gmail.com>
2021-08-13 11:07:35 -0400

    take atomic_weight away from unit tests

--- tests/trainer/test_loss.py
+++ tests/trainer/test_loss.py
@@ -46,70 +46,63 @@ class TestLoss:
         assert isinstance(loss_value, torch.Tensor)
 
 
-class TestWeight:
+class TestNaN:
     def test_loss(self, data):
 
         pred, ref = data
+        print(ref)
 
-        loss = Loss(coeffs=dicts[-1], atomic_weight_on=False)
-        w_loss = Loss(coeffs=dicts[-1], atomic_weight_on=True)
+        loss = Loss(coeffs=dicts[-1])
+        # w_loss = Loss(coeffs=dicts[-1], atomic_weight_on=True)
 
-        w_l, w_contb = w_loss(pred, ref)
-        l, contb = loss(pred, ref)
+        # w_l, w_contb = w_loss(pred, ref)
+        # l, contb = loss(pred, ref)
 
-        assert isinstance(w_l, torch.Tensor)
-        assert not torch.isclose(w_l, l)
-        assert torch.isclose(
-            w_contb[AtomicDataDict.FORCE_KEY], contb[AtomicDataDict.FORCE_KEY]
-        )
+        # assert isinstance(w_l, torch.Tensor)
+        # assert not torch.isclose(w_l, l)
+        # assert torch.isclose(
+        #     w_contb[AtomicDataDict.FORCE_KEY], contb[AtomicDataDict.FORCE_KEY]
+        # )
 
     def test_per_specie(self, data):
 
         pred, ref = data
 
         config = {AtomicDataDict.FORCE_KEY: (1.0, "PerSpeciesMSELoss")}
-        loss = Loss(coeffs=config, atomic_weight_on=False)
-        w_loss = Loss(coeffs=config, atomic_weight_on=True)
+        loss = Loss(coeffs=config)
+        # w_loss = Loss(coeffs=config, atomic_weight_on=True)
 
-        w_l, w_contb = w_loss(pred, ref)
-        l, contb = loss(pred, ref)
+        # w_l, w_contb = w_loss(pred, ref)
+        # l, contb = loss(pred, ref)
 
-        # first half data are specie 1
-        # loss_ref_1 = torch.square(pred[AtomicDataDict.FORCE_KEY][:5] - ref[AtomicDataDict.FORCE_KEY][:5]).mean()
-        # loss_ref_0 = torch.square(pred[AtomicDataDict.FORCE_KEY][5:] - ref[AtomicDataDict.FORCE_KEY][5:]).mean()
+        # # first half data are specie 1
+        # # loss_ref_1 = torch.square(pred[AtomicDataDict.FORCE_KEY][:5] - ref[AtomicDataDict.FORCE_KEY][:5]).mean()
+        # # loss_ref_0 = torch.square(pred[AtomicDataDict.FORCE_KEY][5:] - ref[AtomicDataDict.FORCE_KEY][5:]).mean()
 
-        # since atomic weights are all the same value,
-        # the two loss should have the same result
-        assert isinstance(w_l, torch.Tensor)
-        print(w_l)
-        print(l)
-        assert torch.isclose(w_l, l)
+        # # since atomic weights are all the same value,
+        # # the two loss should have the same result
+        # assert isinstance(w_l, torch.Tensor)
+        # print(w_l)
+        # print(l)
+        # assert torch.isclose(w_l, l)
 
-        for c in [w_contb, contb]:
-            for key, value in c.items():
-                assert key in [AtomicDataDict.FORCE_KEY]
+        # for c in [w_contb, contb]:
+        #     for key, value in c.items():
+        #         assert key in [AtomicDataDict.FORCE_KEY]
 
-        assert torch.allclose(
-            w_contb[AtomicDataDict.FORCE_KEY], contb[AtomicDataDict.FORCE_KEY]
-        )
-        # assert torch.isclose(w_contb[1][AtomicDataDict.FORCE_KEY], loss_ref_1)
-        # assert torch.isclose(w_contb[0][AtomicDataDict.FORCE_KEY], loss_ref_0)
+        # assert torch.allclose(
+        #     w_contb[AtomicDataDict.FORCE_KEY], contb[AtomicDataDict.FORCE_KEY]
+        # )
+        # # assert torch.isclose(w_contb[1][AtomicDataDict.FORCE_KEY], loss_ref_1)
+        # # assert torch.isclose(w_contb[0][AtomicDataDict.FORCE_KEY], loss_ref_0)
 
 
 @pytest.fixture(scope="class")
 def loss(request):
     """"""
     d = request.param
-    instance = Loss(coeffs=d, atomic_weight_on=False)
+    instance = Loss(coeffs=d)
     yield instance
-
-
-@pytest.fixture(scope="class")
-def w_loss():
-    """"""
-    instance = Loss(coeffs=dicts[-1], atomic_weight_on=True)
-    yield instance
-
 
 @pytest.fixture(scope="module")
 def data(float_tolerance):
