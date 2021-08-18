2ca9d3df57ab6d240a66f5d9a3bc572b9a3ed931
Linux-cpp-lisp <1473644+Linux-cpp-lisp@users.noreply.github.com>
2021-08-18 17:59:42 -0400

    test subclass specific key

--- tests/unit/data/test_dataset.py
+++ tests/unit/data/test_dataset.py
@@ -139,18 +139,23 @@ class TestStatistics:
 class TestReload:
     @pytest.mark.parametrize("change_rmax", [0, 1])
     @pytest.mark.parametrize("give_url", [True, False])
-    def test_reload(self, npz_dataset, npz_data, change_rmax, give_url):
+    @pytest.mark.parametrize("change_key_map", [True, False])
+    def test_reload(self, npz_dataset, npz_data, change_rmax, give_url, change_key_map):
         r_max = npz_dataset.extra_fixed_fields["r_max"] + change_rmax
+        keymap = npz_dataset.key_mapping.copy()  # the default one
+        if change_key_map:
+            keymap["x1"] = "x2"
         a = NpzDataset(
             file_name=npz_data,
             root=npz_dataset.root,
             extra_fixed_fields={"r_max": r_max},
+            key_mapping=keymap,
             **({"url": "example.com/data.dat"} if give_url else {})
         )
         print(a.processed_file_names[0])
         print(npz_dataset.processed_file_names[0])
         assert (a.processed_dir == npz_dataset.processed_dir) == (
-            change_rmax == 0 and not give_url
+            (change_rmax == 0) and (not give_url) and (not change_key_map)
         )
 
 
