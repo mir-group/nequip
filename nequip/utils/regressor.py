84b19f2da2ef2971ee28df78723e43d1bb705f4f
nw13slx <nw13mifaso@gmail.com>
2021-08-26 16:57:15 -0400

    debug

--- nequip/utils/regressor.py
+++ nequip/utils/regressor.py
@@ -1,3 +1,4 @@
+import logging
 import torch
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import DotProduct
@@ -6,12 +7,20 @@ from sklearn.gaussian_process.kernels import DotProduct
 def gp(X, y, alpha):
 
     kernel = DotProduct(sigma_0=0, sigma_0_bounds="fixed")
-    gpr = GaussianProcessRegressor(kernel=kernel, random_state=0, alpha=alpha).fit(X, y)
+    print("alpha", alpha)
+    alpha=1
+    gpr = GaussianProcessRegressor(kernel=kernel, random_state=0, alpha=alpha)
+    gpr = gpr.fit(X, y)
+    print(gpr.alpha_)
 
     vec = torch.diag(torch.ones(X.shape[1]))
     mean, std = gpr.predict(vec, return_std=True)
 
     mean = torch.as_tensor(mean, dtype=torch.get_default_dtype()).reshape([-1])
     std = torch.as_tensor(std, dtype=torch.get_default_dtype()).reshape([-1])
+
+    res = torch.sqrt(torch.square(torch.matmul(X, mean.reshape([-1, 1]))-y).mean())
+    logging.debug(f"GP fitting with alpha {alpha}: mean residue {res}")
+    raise RuntimeError()
 
     return mean, std
