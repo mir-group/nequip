9c5d0b818b506a645fc5f299e4442697510effac
nw13slx <nw13mifaso@gmail.com>
2021-09-15 05:18:28 -0400

    refactor str to stat

--- nequip/model/_scaling.py
+++ nequip/model/_scaling.py
@@ -1,10 +1,62 @@
 import logging
 
 import torch
+from torch._C import Value
 
 from nequip.nn import RescaleOutput, GraphModuleMixin, GradientOutput, PerSpeciesScaleShift,
 from nequip.data import AtomicDataDict, AtomicDataset
 from ._grads import ForceOutput
+
+def compute_stats(str_names, dataset, stride):
+
+    # parse the list of string to field, mode 
+    # and record which quantity correspond to which computed_item
+    stat_modes = []
+    stat_fields = []
+    stat_strs = []
+    ids = []
+    tuple_ids = []
+    mode_count = 0
+    tuple_id_map = {"mean":0, "std":1, "rms":0}
+    for name in str_names:
+        # remove dataset prefix
+        if name.startswith("dataset_"):
+            name = name[len("dataset_"):]
+        # identify per_species and per_atom modes
+        prefix = ""
+        if name.startswith("per_species_"):
+            name = name[len("per_species_"):]
+            prefix = "per_species_"
+        elif name.startswith("per_atom_"):
+            name = name[len("per_atom_"):]
+            prefix = "per_atom_"
+        
+        stat = name.split("_")[-1]
+        field = "_".join(name.split("_")[:-1])
+        if stat in ["mean", "std"]:
+            stat_mode = prefix+"mean_std"
+            stat_str = field + prefix + "mean_std"
+        elif stat in ["rms"]:
+            stat_mode = prefix+"rms"
+            stat_str = field + prefix + "rms"
+        else:
+            raise ValueError(f"Cannot handle {stat} type quantity")
+
+        if stat_str in stat_strs:
+            ids += [stat_strs.index(stat_str)]
+        else:
+            ids += [len(stat_strs)]
+            stat_strs += [stat_str]
+            stat_modes += [stat_mode]
+            stat_fields += [field]
+        tuple_ids += [tuple_id_map[stat]]
+
+    values = dataset.statistics(
+        fields=stat_fields,
+        modes=stat_modes,
+        stride=stride,
+    )
+    return [values[idx][tuple_ids[i]] for i, idx in enumerate(ids)]
 
 def RescaleEnergyEtc(
     model: GraphModuleMixin,
@@ -27,55 +79,27 @@ def RescaleEnergyEtc(
 
     # = Get statistics of training dataset =
     if initialize:
-        stats = set()
-        if global_scale == "dataset_energy_std":
-            stats.add((AtomicDataDict.TOTAL_ENERGY_KEY, "mean_std"))
-        elif global_scale == "dataset_force_rms":
-            stats.add((AtomicDataDict.FORCE_KEY, "rms"))
-        elif (
-            global_scale is None
-            or isinstance(global_scale, float)
-            or isinstance(global_scale, torch.Tensor)
-        ):
-            # valid values
-            pass
-        else:
-            raise ValueError(f"Invalid global scale `{global_scale}`")
-
-        if global_shift == "dataset_energy_mean":
-            stats.add((AtomicDataDict.TOTAL_ENERGY_KEY, "mean_std"))
-        elif (
-            global_shift is None
-            or isinstance(global_shift, float)
-            or isinstance(global_shift, torch.Tensor)
-        ):
-            # valid values
-            pass
-        else:
-            raise ValueError(f"Invalid global shift `{global_shift}`")
+        str_names = []
+        for value in [global_scale, global_shift]:
+            if isinstance(value, str):
+                str_names += [value]
+            elif (
+                global_scale is None
+                or isinstance(value, float)
+                or isinstance(value, torch.Tensor)
+            ):
+                # valid values
+                pass
+            else:
+                raise ValueError(f"Invalid global scale `{global_scale}`")
 
         # = Compute shifts and scales =
-        stats = list(stats)
-        computed_stats = dataset.statistics(
-            fields=[e[0] for e in stats],
-            modes=[e[1] for e in stats],
-            stride=config.dataset_statistics_stride,
-        )
+        computed_stats = compute_stats(str_names=str_names, dataset=dataset, stride=config.dataset_statistics_stride)
 
-        def _find_stat(field, mode):
-            return next(
-                computed_stats[i]
-                for i, (this_field, this_mode) in enumerate(stats)
-                if this_field == field and this_mode == mode
-            )
-
-        if global_scale == "dataset_energy_std":
-            global_scale = _find_stat(AtomicDataDict.TOTAL_ENERGY_KEY, "mean_std")[1]
-        elif global_scale == "dataset_force_rms":
-            global_scale = _find_stat(AtomicDataDict.FORCE_KEY, "rms")[0]
-
-        if global_shift == "dataset_energy_mean":
-            global_shift = _find_stat(AtomicDataDict.TOTAL_ENERGY_KEY, "mean_std")[0]
+        if isinstance(global_scale, str):
+            global_scale = computed_stats[str_names.index(global_scale)]
+        if isinstance(global_shift, str):
+            global_shift = computed_stats[str_names.index(global_shift)]
 
         RESCALE_THRESHOLD = 1e-6
         if isinstance(global_scale, float) and global_scale < RESCALE_THRESHOLD:
@@ -129,6 +153,7 @@ def PerSpecieRescale(
 
     If ``initialize`` is false, doesn't compute statistics.
     """
+    module_prefix = "PerSpeciesScaleShift_"
 
     force_training = AtomicDataDict.FORCE_KEY in model.irreps_out
 
@@ -137,44 +162,56 @@ def PerSpecieRescale(
         "global_rescale_scale",
         "dataset_force_rms" if force_training else "dataset_energy_std",)
     global_shift = config.get("global_rescale_shift", None)
-    per_species_scale_shift = get_per_species(config, "enable", False)
-    scales = pop_per_species(config, "scales", None)
-    shifts = pop_per_species(config, "shifts", None)
+    scales = config.get(module_prefix+"scales", None)
+    shifts = config.get(module_prefix+"shifts", None)
 
-    if global_shift is not None and per_species_scale_shift:
+    if global_shift is not None:
         raise ValueError("One can only enable either global shift or per_species shift")
-    logging.info(f"Enable per species scale/shift: {per_species_scale_shift}")
+
+    logging.info(f"Enable per species scale/shift")
 
     # = Determine what statistics need to be compute =
-    keys = []
-    for variable in [global_shift, global_scale, scales, shifts]:
-        if isinstance(variable, str) and variable.startswith("dataset"):
-            keys += [variable[len("dataset_") :]]
-    keys = list(set(keys))
-    if "force_rms" in keys and not force_training:
-        raise ValueError(
-            "Cannot have global_scale = 'dataset_force_rms' without force training"
-        )
+    if initialize:
+        str_names = []
+        for value in [scales, shifts, global_scale]:
+            if isinstance(value, str):
+                str_names += [value]
+            elif (
+                global_scale is None
+                or isinstance(value, float)
+                or isinstance(value, list)
+                or isinstance(value, torch.Tensor)
+            ):
+                # valid values
+                pass
+            else:
+                raise ValueError(f"Invalid global scale `{global_scale}`")
 
-    # = Get statistics of training dataset =
-    dataset_statistics = {}
-    if "force_rms" in keys:
-        ((rms,),) = trainer.dataset_train.statistics(
-            fields=[AtomicDataDict.FORCE_KEY],
-            modes=["rms"],
-            stride=config.dataset_statistics_stride,
-        )
-        dataset_statistics["dataset_force_rms"] = rms
-    if "energy_std" in keys or "energy_mean" in keys:
-        ((mean, std),) = trainer.dataset_train.statistics(
-            fields=[AtomicDataDict.TOTAL_ENERGY_KEY],
+        # = Compute shifts and scales =
+        computed_stats = compute_stats(str_names=str_names, dataset=dataset, stride=config.dataset_statistics_stride)
+
+        if isinstance(scales, str):
+            scales = computed_stats[str_names.index(scales)]
+        if isinstance(shifts, str):
+            shifts = computed_stats[str_names.index(shifts)]
+        if isinstance(global_scale, str):
+            global_scale = computed_stats[str_names.index(global_scale)]
+        
+        if global_scale is not None:
+            scales /= global_scale
+
+    else:
+        # Put dummy values
+        if scales is not None:
+            scales = 1.0  # it has some kind of value
+        if shifts is not None:
+            shifts = 1.0  # same,
 
     # first peel off the gradient part
-    if force_training:
-        model = model.func
+    model_func = model.func if force_training else model
     
     # insert in per species shift
-    model.insert_from_parameters(
+    model_func.insert_from_parameters(
         after="total_energy_sum",
         shared_params=config,
         name="per_species_scale_shift",
@@ -184,10 +221,6 @@ def PerSpecieRescale(
             out_field=AtomicDataDict.PER_ATOM_ENERGY_KEY,
         ),
         prepend=True,)
-
-    # wrap it back with gradient
-    if force_training:
-        model = ForceOutput(model)
 
     # == Build the model ==
     return RescaleOutput(
@@ -211,29 +244,3 @@ def PerSpecieRescale(
             "trainable_global_rescale_scale", False
         ),
     )
-
-def pop_per_species(config, key, default):
-    return config.pop(
-        f"PerSpeciesScaleShift_{key}",
-        config.pop(f"per_species_scale_shift_{key}", default),
-    )
-
-
-def get_per_species(config, key, default):
-    return config.get(
-        f"PerSpeciesScaleShift_{key}",
-        config.get(f"per_species_scale_shift_{key}", default),
-    )
-
-
-def set_value(variable, variable_name, value_dict):
-    for key, value in value_dict.items():
-        if variable == key:
-            return value
-    if (
-        variable is None
-        or isinstance(variable, float)
-        or isinstance(variable, torch.Tensor)
-    ):
-        return variable
-    raise ValueError(f"Invalid {variable_name} `{variable}`")
