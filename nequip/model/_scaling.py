623c5105c47e921b7050efed7024f925a2bb289a
nw13slx <nw13mifaso@gmail.com>
2021-09-15 22:26:45 -0400

    add compute_stat function

--- nequip/model/_scaling.py
+++ nequip/model/_scaling.py
@@ -1,9 +1,12 @@
 import logging
+from typing import List
 
 import torch
+from torch._C import Value
 
-from nequip.nn import RescaleOutput, GraphModuleMixin
+from nequip.nn import RescaleOutput, GraphModuleMixin, PerSpeciesScaleShift
 from nequip.data import AtomicDataDict, AtomicDataset
+from ._grads import ForceOutput
 
 
 def RescaleEnergyEtc(
@@ -16,66 +19,43 @@ def RescaleEnergyEtc(
 
     If ``initialize`` is false, doesn't compute statistics.
     """
+
     global_scale = config.get(
         "global_rescale_scale",
-        "dataset_force_rms"
+        f"dataset_{AtomicDataDict.FORCE_KEY}_rms"
         if AtomicDataDict.FORCE_KEY in model.irreps_out
-        else "dataset_energy_std",
+        else f"dataset_{AtomicDataDict.TOTAL_ENERGY_KEY}_std",
     )
     # TODO: change this default?
-    global_shift = config.get("global_rescale_shift", "dataset_energy_mean")
+    global_shift = config.get("global_rescale_shift", f"dataset_{AtomicDataDict.TOTAL_ENERGY_KEY}_mean")
 
     # = Get statistics of training dataset =
     if initialize:
-        stats = set()
-        if global_scale == "dataset_energy_std":
-            stats.add((AtomicDataDict.TOTAL_ENERGY_KEY, "mean_std"))
-        elif global_scale == "dataset_force_rms":
-            stats.add((AtomicDataDict.FORCE_KEY, "rms"))
-        elif (
-            global_scale is None
-            or isinstance(global_scale, float)
-            or isinstance(global_scale, torch.Tensor)
-        ):
-            # valid values
-            pass
-        else:
-            raise ValueError(f"Invalid global scale `{global_scale}`")
-
-        if global_shift == "dataset_energy_mean":
-            stats.add((AtomicDataDict.TOTAL_ENERGY_KEY, "mean_std"))
-        elif (
-            global_shift is None
-            or isinstance(global_shift, float)
-            or isinstance(global_shift, torch.Tensor)
-        ):
-            # valid values
-            pass
-        else:
-            raise ValueError(f"Invalid global shift `{global_shift}`")
+        str_names = []
+        for value in [global_scale, global_shift]:
+            if isinstance(value, str):
+                str_names += [value]
+            elif (
+                global_scale is None
+                or isinstance(value, float)
+                or isinstance(value, torch.Tensor)
+            ):
+                # valid values
+                pass
+            else:
+                raise ValueError(f"Invalid global scale `{global_scale}`")
 
         # = Compute shifts and scales =
-        stats = list(stats)
-        computed_stats = dataset.statistics(
-            fields=[e[0] for e in stats],
-            modes=[e[1] for e in stats],
+        computed_stats = compute_stats(
+            str_names=str_names,
+            dataset=dataset,
             stride=config.dataset_statistics_stride,
         )
 
-        def _find_stat(field, mode):
-            return next(
-                computed_stats[i]
-                for i, (this_field, this_mode) in enumerate(stats)
-                if this_field == field and this_mode == mode
-            )
-
-        if global_scale == "dataset_energy_std":
-            global_scale = _find_stat(AtomicDataDict.TOTAL_ENERGY_KEY, "mean_std")[1]
-        elif global_scale == "dataset_force_rms":
-            global_scale = _find_stat(AtomicDataDict.FORCE_KEY, "rms")[0]
-
-        if global_shift == "dataset_energy_mean":
-            global_shift = _find_stat(AtomicDataDict.TOTAL_ENERGY_KEY, "mean_std")[0]
+        if isinstance(global_scale, str):
+            global_scale = computed_stats[str_names.index(global_scale)]
+        if isinstance(global_shift, str):
+            global_shift = computed_stats[str_names.index(global_shift)]
 
         RESCALE_THRESHOLD = 1e-6
         if isinstance(global_scale, float) and global_scale < RESCALE_THRESHOLD:
@@ -92,6 +72,10 @@ def RescaleEnergyEtc(
             global_shift = 0.0  # it has some kind of value
         if global_scale is not None:
             global_scale = 1.0  # same,
+
+    logging.info(
+        f"Initially outputs are scaled by: {global_scale}, eneriges are shifted by {global_shift}."
+    )
 
     # == Build the model ==
     return RescaleOutput(
@@ -117,3 +101,67 @@ def RescaleEnergyEtc(
             "trainable_global_rescale_scale", False
         ),
     )
+
+
+def compute_stats(str_names:List[str], dataset, stride: int):
+    """return the values of statistics over dataset
+    quantity name should be dataset_key_stat, where key can be any key
+    that exists in the dataset, stat can be mean, std
+
+    Args:
+
+    str_names: list of strings that define the quantity to compute
+    dataset: dataset object to run the stats over
+    stride: # frames to skip for every one frame to include
+    """
+
+    # parse the list of string to field, mode
+    # and record which quantity correspond to which computed_item
+    stat_modes = []
+    stat_fields = []
+    stat_strs = []
+    ids = []
+    tuple_ids = []
+    tuple_id_map = {"mean": 0, "std": 1, "rms": 0}
+    for name in str_names:
+
+        # remove dataset prefix
+        if name.startswith("dataset_"):
+            name = name[len("dataset_") :]
+        # identify per_species and per_atom modes
+        prefix = ""
+        if name.startswith("per_species_"):
+            name = name[len("per_species_") :]
+            prefix = "per_species_"
+        elif name.startswith("per_atom_"):
+            name = name[len("per_atom_") :]
+            prefix = "per_atom_"
+
+        stat = name.split("_")[-1]
+        field = "_".join(name.split("_")[:-1])
+        if stat in ["mean", "std"]:
+            stat_mode = prefix + "mean_std"
+            stat_str = field + prefix + "mean_std"
+        elif stat in ["rms"]:
+            stat_mode = prefix + "rms"
+            stat_str = field + prefix + "rms"
+        else:
+            raise ValueError(f"Cannot handle {stat} type quantity")
+
+        if stat_str in stat_strs:
+            ids += [stat_strs.index(stat_str)]
+        else:
+            ids += [len(stat_strs)]
+            stat_strs += [stat_str]
+            stat_modes += [stat_mode]
+            stat_fields += [field]
+        tuple_ids += [tuple_id_map[stat]]
+
+    values = dataset.statistics(
+        fields=stat_fields,
+        modes=stat_modes,
+        stride=stride,
+    )
+    return [values[idx][tuple_ids[i]] for i, idx in enumerate(ids)]
+
+
