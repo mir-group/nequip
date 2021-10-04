09b03a7e45f07bccd22af06e3c7705525f9df1fa
Linux-cpp-lisp <1473644+Linux-cpp-lisp@users.noreply.github.com>
2021-10-04 15:53:34 -0400

    import

--- tests/unit/utils/test_tests.py
+++ tests/unit/utils/test_tests.py
@@ -4,13 +4,11 @@ import torch
 
 from e3nn import o3
 
-from nequip.data import AtomicDataDict
+from nequip.data import AtomicDataDict, register_fields, deregister_fields
 from nequip.nn import GraphModuleMixin
 from nequip.utils.test import (
     assert_AtomicData_equivariant,
     assert_permutation_equivariant,
-    register_fields,
-    deregister_fields,
 )
 
 
