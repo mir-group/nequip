0cdbacaf854d9e14b3b1b9830f23e9b4887fc81f
nw13slx <nw13mifaso@gmail.com>
2022-01-22 15:56:45 -0500

    fix bondary condition

--- nequip/nn/_graph_mixin.py
+++ nequip/nn/_graph_mixin.py
@@ -295,7 +295,7 @@ class SequentialGraphNetwork(GraphModuleMixin, torch.nn.Sequential):
             assert AtomicDataDict._irreps_compatible(
                 module_list[idx - 1].irreps_out, module.irreps_in
             )
-        if len(module_list) > idx:
+        if len(module_list) - 1 > idx:
             assert AtomicDataDict._irreps_compatible(
                 module_list[idx + 1].irreps_in, module.irreps_out
             )
