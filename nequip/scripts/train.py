4ecb6555bba065f723b755185da804986dda0149
nw13slx <nw13mifaso@gmail.com>
2022-01-26 16:58:03 -0500

    change import so the script can be run from anywhere

--- nequip/scripts/train.py
+++ nequip/scripts/train.py
@@ -20,7 +20,7 @@ from nequip.utils import Config
 from nequip.data import dataset_from_config
 from nequip.utils.test import assert_AtomicData_equivariant, set_irreps_debug
 from nequip.utils import load_file, dtype_from_name
-from ._logger import set_up_script_logger
+from nequip.scripts._logger import set_up_script_logger
 
 default_config = dict(
     root="./",
