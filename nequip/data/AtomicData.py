d174a296d11b06b3beec622a4cf97b2ab9eaa53d
Linux-cpp-lisp <1473644+Linux-cpp-lisp@users.noreply.github.com>
2022-02-08 13:28:08 -0500

    better error

--- nequip/data/AtomicData.py
+++ nequip/data/AtomicData.py
@@ -477,20 +477,16 @@ class AtomicData(Data):
         force = getattr(self, AtomicDataDict.FORCE_KEY, None)
         do_calc = energy is not None or force is not None
 
+        # exclude those that are special for ASE and that we process seperately
+        special_handling_keys = [
+            AtomicDataDict.POSITIONS_KEY,
+            AtomicDataDict.CELL_KEY,
+            AtomicDataDict.PBC_KEY,
+            AtomicDataDict.ATOMIC_NUMBERS_KEY,
+        ] + AtomicDataDict.ALL_ENERGY_KEYS
         assert (
-            len(
-                set(extra_fields).intersection(
-                    [  # exclude those that are special for ASE and that we process seperately
-                        AtomicDataDict.POSITIONS_KEY,
-                        AtomicDataDict.CELL_KEY,
-                        AtomicDataDict.PBC_KEY,
-                        AtomicDataDict.ATOMIC_NUMBERS_KEY,
-                    ]
-                    + AtomicDataDict.ALL_ENERGY_KEYS
-                )
-            )
-            == 0
-        ), "Cannot specify typical keys as `extra_fields` for atoms output"
+            len(set(extra_fields).intersection(special_handling_keys)) == 0
+        ), f"Cannot specify keys handled in special ways ({special_handling_keys}) as `extra_fields` for atoms output--- they are output by default"
 
         if cell is not None:
             cell = cell.view(-1, 3, 3)
