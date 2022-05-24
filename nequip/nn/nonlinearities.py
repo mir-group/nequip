9424adac3438d7ee4f96c53cd1e634e407b9b2dc
Linux-cpp-lisp <1473644+Linux-cpp-lisp@users.noreply.github.com>
2022-05-23 21:55:38 -0400

    still compile ssp

--- nequip/nn/nonlinearities.py
+++ nequip/nn/nonlinearities.py
@@ -5,7 +5,12 @@ import math
 from e3nn.util.jit import compile_mode
 
 
+@torch.jit.script
+def shifted_soft_plus(x: torch.Tensor) -> torch.Tensor:
+    return torch.nn.functional.softplus(x) - math.log(2.0)
+
+
 @compile_mode("script")
 class ShiftedSoftPlus(torch.nn.Module):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return torch.nn.functional.softplus(x) - math.log(2.0)
+        return shifted_soft_plus(x)
