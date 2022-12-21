fix type promotion in scaling

nequip/nn/_atomwise.py
@@ -205,6 +205,7 @@ class PerSpeciesScaleShift(GraphModuleMixin, torch.nn.Module):
             # we can used an FMA for performance
             # addcmul computes
             # input + tensor1 * tensor2 elementwise
+            # it will promote to widest dtype, which comes from shifts/scales
             in_field = torch.addcmul(
                 self.shifts[species_idx].view(-1, 1),
                 self.scales[species_idx].view(-1, 1),
@@ -212,7 +213,9 @@ class PerSpeciesScaleShift(GraphModuleMixin, torch.nn.Module):
             )
         else:
             # fallback path for torch<1.13 OR mix of enabled shifts and scales
-            # multiplication / addition promotes dtypes already, so no cast is needed:
+            # multiplication / addition promotes dtypes already, so no cast is needed
+            # this is specifically because self.*[species_idx].view(-1, 1)
+            # is never a scalar (ndim == 0), since it is always [n_atom, 1]
             if self.has_scales:
                 in_field = self.scales[species_idx].view(-1, 1) * in_field
             if self.has_shifts:

nequip/nn/_rescale.py
@@ -38,6 +38,7 @@ class RescaleOutput(GraphModuleMixin, torch.nn.Module):
     related_shift_keys: List[str]
     scale_trainble: bool
     rescale_trainable: bool
+    _all_keys: List[str]
 
     has_scale: bool
     has_shift: bool
@@ -87,6 +88,7 @@ class RescaleOutput(GraphModuleMixin, torch.nn.Module):
 
         self.scale_keys = list(scale_keys)
         self.shift_keys = list(shift_keys)
+        self._all_keys = list(all_keys)
         self.related_scale_keys = list(set(related_scale_keys).union(scale_keys))
         self.related_shift_keys = list(set(related_shift_keys).union(shift_keys))
 
@@ -149,17 +151,32 @@ class RescaleOutput(GraphModuleMixin, torch.nn.Module):
     def forward(self, data: AtomicDataDict.Type) -> AtomicDataDict.Type:
         data = self.model(data)
         if self.training:
-            return data
+            # no scaling, but still need to promote for consistent dtype behavior
+            # this is hopefully a no-op in most circumstances due to a
+            # preceeding PerSpecies rescaling:
+            for field in self._all_keys:
+                data[field] = data[field].to(dtype=self.default_dtype)
         else:
             # Scale then shift
-            # * and + promote dtypes by default
+            # * and + promote dtypes by default, but not when the other
+            # operand is a scalar, which `scale/shift_by` are.
+            # The .to(dtype=self.default_dtype) should be a free no-op
+            # under most circumstances, since if this RescaleOutput
+            # is preceeded by a PerSpecies, that will cast up to
+            # default_dtype through promotion in the per-atom
+            # * and +, which are always between tensors and always
+            # promote. Still, we include it just to be sure
             if self.has_scale:
                 for field in self.scale_keys:
-                    data[field] = data[field] * self.scale_by
+                    data[field] = (
+                        data[field].to(dtype=self.default_dtype) * self.scale_by
+                    )
             if self.has_shift:
                 for field in self.shift_keys:
-                    data[field] = data[field] + self.shift_by
-            return data
+                    data[field] = (
+                        data[field].to(dtype=self.default_dtype) + self.shift_by
+                    )
+        return data
 
     @torch.jit.export
     def scale(

nequip/train/trainer.py
@@ -658,6 +658,19 @@ class Trainer:
         device="cpu",
         config_dictionary: Optional[dict] = None,
     ) -> Tuple[torch.nn.Module, Config]:
+        """Load a model from a training session.
+
+        Note that this uses ``model_from_config`` internally and is thus not thread safe.
+
+        Args:
+            traindir: the training session
+            model_name: which checkpoint to load; defaults to ``best_model.pth``
+            device: target device to load to, defaults to ``cpu``
+            config_dictionary: optionally use this config instead of ``traindir/config.yaml``
+
+        Returns:
+            (model, config)
+        """
         traindir = str(traindir)
         model_name = str(model_name)
 
@@ -666,21 +679,14 @@ class Trainer:
         else:
             config = Config.from_file(traindir + "/config.yaml")
 
+        # model_from_config takes care of dtypes already
         model = model_from_config(
             config=config,
             initialize=False,
         )
-        if model is not None:  # TODO: why would it be?
-            # TODO: this is not exactly equivalent to building with
-            # this set as default dtype... does it matter?
-            model.to(
-                device=torch.device(device),
-                dtype=dtype_from_name(config.default_dtype),
-            )
-            model_state_dict = torch.load(
-                traindir + "/" + model_name, map_location=device
-            )
-            model.load_state_dict(model_state_dict)
+        model.to(device=torch.device(device))
+        model_state_dict = torch.load(traindir + "/" + model_name, map_location=device)
+        model.load_state_dict(model_state_dict)
 
         return model, config
 

tests/integration/test_deploy.py
@@ -19,13 +19,13 @@ from nequip.ase import NequIPCalculator
 @pytest.mark.parametrize(
     "device", ["cpu"] + (["cuda"] if torch.cuda.is_available() else [])
 )
-def test_deploy(BENCHMARK_ROOT, device):
+@pytest.mark.parametrize("model_dtype", ["float32", "float64"])
+def test_deploy(BENCHMARK_ROOT, device, model_dtype):
     dtype = str(torch.get_default_dtype())[len("torch.") :]
-    atol = {"float32": 1e-5, "float64": 1e-7}[dtype]
-
-    # if torch.cuda.is_available():
-    #     # TODO: is this true?
-    #     pytest.skip("CUDA and subprocesses have issues")
+    if dtype == "float32" and model_dtype == "float64":
+        pytest.skip("default_dtype=float32 and model_dtype=float64 doesn't make sense")
+    # atol on MODEL dtype, since a mostly float32 model still has float32 variation
+    atol = {"float32": 1e-5, "float64": 1e-7}[model_dtype]
 
     keys = [
         AtomicDataDict.TOTAL_ENERGY_KEY,
@@ -45,6 +45,7 @@ def test_deploy(BENCHMARK_ROOT, device):
             BENCHMARK_ROOT / "aspirin_ccsd-train.npz"
         )
         true_config["default_dtype"] = dtype
+        true_config["model_dtype"] = model_dtype
         true_config["max_epochs"] = 1
         true_config["n_train"] = 1
         true_config["n_val"] = 1
@@ -72,7 +73,7 @@ def test_deploy(BENCHMARK_ROOT, device):
         assert deployed_path.is_file(), "Deploy didn't create file"
 
         # now test predictions the same
-        best_mod, _ = Trainer.load_model_from_training_session(
+        best_mod, train_config = Trainer.load_model_from_training_session(
             traindir=f"{root}/{run_name}/",
             model_name="best_model.pth",
             device=device,