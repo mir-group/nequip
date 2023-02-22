8c150d9f87152260d4b2f1758b3a3fab28344944
Albert Zhu <axzhu@college.harvard.edu>
2023-02-21 20:22:00 -0500

    complete draft of gmm that passes simple fitting and scoring test

--- nequip/utils/gmm.py
+++ nequip/utils/gmm.py
@@ -2,7 +2,60 @@ from typing import Optional
 
 import torch
 import numpy as np
-from sklearn.mixture import GaussianMixture
+from sklearn import mixture
+
+
+@torch.jit.script
+def _compute_log_det_cholesky(
+    matrix_chol: torch.Tensor, covariance_type: str, n_features: int
+):
+    """Compute the log-det of the cholesky decomposition of matrices."""
+
+    assert covariance_type in ("full",)
+    n_components = matrix_chol.size(dim=0)
+    log_det_chol = torch.sum(
+        torch.log(matrix_chol.view(n_components, -1)[:, :: n_features + 1]), dim=1
+    )
+
+    return log_det_chol
+
+
+@torch.jit.script
+def _estimate_log_gaussian_prob(
+    X: torch.Tensor,
+    means: torch.Tensor,
+    precisions_chol: torch.Tensor,
+    covariance_type: str,
+):
+    """Estimate the log Gaussian probability."""
+
+    assert covariance_type in ("full",)
+    n_samples, n_features = X.size(dim=0), X.size(dim=1)
+    n_components = means.size(dim=0)
+
+    # det(precision_chol) = -0.5 * det(precision)
+    log_det = _compute_log_det_cholesky(precisions_chol, covariance_type, n_features)
+
+    # TODO: understand this
+    log_prob = torch.empty((n_samples, n_components))
+    for k, (mu, prec_chol) in enumerate(zip(means, precisions_chol)):
+        y = torch.mm(X, prec_chol) - torch.mm(mu.unsqueeze(0), prec_chol)
+        log_prob[:, k] = torch.sum(torch.square(y), dim=1)
+    # X = X.unsqueeze(0)
+    # means = means.unsqueeze(2)
+    # print(f"X shape: {X.size()}")
+    # print(f"means shape: {means.size()}")
+    # log_prob = torch.sum(
+    #     torch.square(
+    #         torch.matmul(X, precisions_chol.transpose(1, 2))
+    #         - torch.matmul(means, precisions_chol.transpose(1, 2))
+    #     ),
+    #     dim=1,
+    # )
+
+    # Since we are using the precision of the Cholesky decomposition,
+    # `-0.5 * log_det` becomes `+ log_det`
+    return -0.5 * (n_features * torch.log(torch.tensor(2 * np.pi)) + log_prob) + log_det
 
 
 class GaussianMixture(torch.nn.Module):
@@ -23,7 +76,7 @@ class GaussianMixture(torch.nn.Module):
         n_features: int,
         covariance_type: str = "full",
     ):
-        super(self).__init__()
+        super(GaussianMixture, self).__init__()
         assert covariance_type in ("full",)
         self.covariance_type = covariance_type
         self.n_components = n_components
@@ -36,9 +89,6 @@ class GaussianMixture(torch.nn.Module):
             "covariances", torch.zeros(n_components, n_features, n_features)
         )
         self.register_buffer(
-            "precisions", torch.zeros(n_components, n_features, n_features)
-        )
-        self.register_buffer(
             "precisions_cholesky", torch.zeros(n_components, n_features, n_features)
         )
         # TODO: other parameters as buffers
@@ -46,7 +96,17 @@ class GaussianMixture(torch.nn.Module):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """Compute the NLL of samples ``x`` under the GMM."""
         # TODO: implement
-        return x
+
+        # TODO: check if model has been fitted
+
+        # TODO: testing
+        estimated_log_probs = _estimate_log_gaussian_prob(
+            x, self.means, self.precisions_cholesky, self.covariance_type
+        )
+
+        estimated_weights = np.log(self.weights)
+
+        return torch.logsumexp(estimated_log_probs + estimated_weights, dim=1)
 
     @torch.jit.unused
     def fit(self, x: torch.Tensor) -> None:
@@ -61,7 +121,7 @@ class GaussianMixture(torch.nn.Module):
             tmp_heurstic = 30  # TODO: determine how to set max number of components for BIC testing
             components = np.arange(1, tmp_heurstic)
             gmms = [
-                GaussianMixture(
+                mixture.GaussianMixture(
                     n_components=n, covariance_type="full", random_state=27617
                 )
                 for n in components
@@ -71,7 +131,7 @@ class GaussianMixture(torch.nn.Module):
 
         # Fit GMM
         random_state = self.seed if self.seed else 123
-        gmm = GaussianMixture(
+        gmm = mixture.GaussianMixture(
             n_components=n_components,
             covariance_type=self.covariance_type,
             random_state=random_state,
@@ -82,7 +142,8 @@ class GaussianMixture(torch.nn.Module):
         self.register_buffer("means", torch.from_numpy(gmm.means_))
         self.register_buffer("weights", torch.from_numpy(gmm.weights_))
         self.register_buffer("covariances", torch.from_numpy(gmm.covariances_))
-        self.register_buffer("precisions", torch.from_numpy(gmm.precisions_))
         self.register_buffer(
-            "precisions_cholesky", torch.from_numpy(gmm.precisions_cholesky)
+            "precisions_cholesky", torch.from_numpy(gmm.precisions_cholesky_)
         )
+
+        # TODO: replace above code with self.means[:] = gmm.means_ ?
--- tests/unit/utils/test_gmm.py
+++ tests/unit/utils/test_gmm.py
@@ -1,4 +1,56 @@
-from nequip.utils.gmm import GaussianMixture
+import torch
+import numpy as np
+from nequip.utils import gmm
+from sklearn import mixture
+
+fit_data = np.random.rand(10, 3)
+gmm_sklearn = mixture.GaussianMixture(
+    n_components=3, covariance_type="full", random_state=123
+)
+
+gmm_torch = gmm.GaussianMixture(n_components=3, seed=123, n_features=3)
+# gmm_torch = torch.jit.script(gmm_torch)
+
+
+class TestGMM:
+    def test_fit_forward_simple(self):
+        gmm_sklearn.fit(fit_data)
+        gmm_torch.fit(torch.from_numpy(fit_data))
+
+        assert torch.allclose(
+            torch.from_numpy(gmm_sklearn.means_), gmm_torch.state_dict()["means"]
+        )
+
+        assert torch.allclose(
+            torch.from_numpy(gmm_sklearn.covariances_),
+            gmm_torch.state_dict()["covariances"],
+        )
+
+        assert torch.allclose(
+            torch.from_numpy(gmm_sklearn.weights_), gmm_torch.state_dict()["weights"]
+        )
+
+        assert torch.allclose(
+            torch.from_numpy(gmm_sklearn.precisions_cholesky_),
+            gmm_torch.state_dict()["precisions_cholesky"],
+        )
+
+        test_data = np.random.rand(20, 3)
+
+        sklearn_nll = gmm_sklearn.score_samples(test_data)
+        torch_nll = gmm_torch.forward(torch.from_numpy(test_data))
+
+        print(f"sklearn_nll shape: {sklearn_nll.shape}")
+        print(f"torch_nll shape: {torch_nll.size()}")
+
+        assert torch.allclose(
+            torch.from_numpy(sklearn_nll),
+            torch_nll,
+        )
+
+    def test_forward(self):
+
+        assert True
 
 
 # TODO:  test consistancy of GaussianMixture NLLs with pure sklearn solution
