fix vectorized formula and add back factor of 2

nequip/utils/_global_options.py
@@ -84,10 +84,10 @@ def _set_global_options(config, warn_on_override: bool = False) -> None:
         if version.parse(torch.__version__) >= version.parse("1.12")
         else "fuser1"
     )
-    if torch.cuda.is_available():
-        fuser = config.get("_jit_fuser", default_fuser)
+    fuser = config.get("_jit_fuser", default_fuser)
     # context manager just restores old fuser afterwards
-    torch.jit.fuser(fuser).__enter__()
+    if torch.cuda.is_available():
+        torch.jit.fuser(fuser).__enter__()
     if warn_on_override and fuser != default_fuser:
         # ^ meh assumption, but better than hardcoding getting the old state
         warnings.warn(

nequip/utils/gmm.py
@@ -4,6 +4,7 @@ import math
 import torch
 import numpy as np
 from sklearn import mixture
+from e3nn.util.jit import compile_mode
 
 
 @torch.jit.script
@@ -33,7 +34,8 @@ def _estimate_log_gaussian_prob(
     """Estimate the log Gaussian probability."""
 
     assert covariance_type in ("full",)
-    n_features = X.size(dim=1)
+    n_samples, n_features = X.size(dim=0), X.size(dim=1)
+    n_components = means.size(dim=0)
 
     # det(precision_chol) = -0.5 * det(precision)
     log_det = _compute_log_det_cholesky(precisions_chol, covariance_type, n_features)
@@ -47,12 +49,12 @@ def _estimate_log_gaussian_prob(
     # ALBY'S CODE
     # X [n_sample, n_feature]
     # prec_chol is [n_component, n_feature, n_feature]
-    X_centered = X.unsqueeze(-2) - means.unsqueeze(
-        0
-    )  # [n_sample, 1, n_feature] - [1, n_component, n_feature] = [n_sample, n_component, n_feature]
+    X_centered = X.unsqueeze(-2) - means.unsqueeze(0)
+    print(f"X_centered size: {X_centered.size()}")
+    # [n_sample, 1, n_feature] - [1, n_component, n_feature] = [n_sample, n_component, n_feature]
     # [n_sample, n_component, n_feature] * [n_component, n_feature, n_feature]
     log_prob = (
-        torch.einsum("zci,ij->zcj", X_centered, precisions_chol).square().sum(dim=-1)
+        torch.einsum("zci,cij->zcj", X_centered, precisions_chol).square().sum(dim=-1)
     )
 
     # X = X.unsqueeze(0)
@@ -72,6 +74,7 @@ def _estimate_log_gaussian_prob(
     return -0.5 * (n_features * math.log(2 * math.pi) + log_prob) + log_det
 
 
+@compile_mode("script")
 class GaussianMixture(torch.nn.Module):
     """Calculate NLL of samples under a Gaussian Mixture Model (GMM).
 
@@ -134,6 +137,7 @@ class GaussianMixture(torch.nn.Module):
         n_components = self.n_components
         g = rng if rng else torch.Generator()
         random_state = torch.randint(2**16, (1,), generator=g).item()
+        print(f"torch seed: {random_state}")
         if not n_components:
             components = range(1, max_components)
             gmms = [

tests/unit/utils/test_gmm.py
@@ -5,14 +5,15 @@ from sklearn import mixture
 from e3nn.util.test import assert_auto_jitable
 
 # TODO: use pytest fixtures
-rng = np.random.RandomState(678912345)
-fit_data = rng.rand(10, 3)
+g = torch.Generator().manual_seed(678912345)
+sklearn_seed = torch.randint(2**16, (1,), generator=g).item()
+fit_data_rng = np.random.RandomState(678912345)
+fit_data = fit_data_rng.rand(10, 4)
 gmm_sklearn = mixture.GaussianMixture(
-    n_components=3, covariance_type="full", random_state=rng.rand(1)
+    n_components=3, covariance_type="full", random_state=50993
 )
 
-gmm_torch = gmm.GaussianMixture(n_components=3, n_features=3)
-gmm_torch = torch.jit.script(gmm_torch)
+gmm_torch = gmm.GaussianMixture(n_components=3, n_features=4)
 
 
 class TestGMM:
@@ -20,8 +21,9 @@ class TestGMM:
         assert_auto_jitable(gmm_torch)
 
     def test_fit_forward_simple(self):
+        print(f"sklearn seed: {sklearn_seed}")
         gmm_sklearn.fit(fit_data)
-        gmm_torch.fit(torch.from_numpy(fit_data))
+        gmm_torch.fit(torch.from_numpy(fit_data), rng=g)
 
         assert torch.allclose(torch.from_numpy(gmm_sklearn.means_), gmm_torch.means)
 
@@ -36,13 +38,16 @@ class TestGMM:
             gmm_torch.precisions_cholesky,
         )
 
-        test_data = rng.rand(20, 3)
+        test_data_rng = np.random.RandomState(123456789)
+        test_data = test_data_rng.rand(20, 4)
 
         sklearn_nll = gmm_sklearn.score_samples(test_data)
         torch_nll = gmm_torch(torch.from_numpy(test_data))
 
         print(f"sklearn_nll shape: {sklearn_nll.shape}")
         print(f"torch_nll shape: {torch_nll.size()}")
+        print(f"sklearn nll: {torch.from_numpy(sklearn_nll)}")
+        print(f"torch nll: {torch_nll}")
 
         assert torch.allclose(
             torch.from_numpy(sklearn_nll),