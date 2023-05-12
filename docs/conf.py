c1d7cd92541c418b6577f5bf1a20cd1f8ad7d731
Linux-cpp-lisp <1473644+Linux-cpp-lisp@users.noreply.github.com>
2023-05-12 16:07:49 -0400

    docs, again

--- docs/conf.py
+++ docs/conf.py
@@ -33,6 +33,7 @@ extensions = [
     "sphinx_rtd_theme",
     "myst_parser",
 ]
+source_suffix = [".rst", ".md"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
