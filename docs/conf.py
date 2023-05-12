5bddd740c078882d0666ba5b79a5289ceac54b3b
Linux-cpp-lisp <1473644+Linux-cpp-lisp@users.noreply.github.com>
2023-05-12 16:01:24 -0400

    favicon

--- docs/conf.py
+++ docs/conf.py
@@ -54,8 +54,11 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-
+html_favicon = "favicon.png"
 html_logo = "logo.png"
+html_theme_options = {
+    "logo_only": True,
+}
 
 
 def setup(app):
