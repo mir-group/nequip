25e6320fc017e0fb7c6162eefd10194cedca6397
cw-tan <chuinwei.tan@gmail.com>
2024-07-13 22:57:23 -0400

    separate model_dtype and default_dtype in atomic unittest

--- tests/unit/nn/test_atomic.py
+++ tests/unit/nn/test_atomic.py
@@ -7,15 +7,17 @@ from nequip.nn import (
     AtomwiseReduce,
     PerSpeciesScaleShift,
     SequentialGraphNetwork,
+    GraphModel,
 )
 from nequip.nn.embedding import (
     OneHotAtomEncoding,
 )
 from nequip.utils.torch_geometric import Batch
+from nequip.utils import dtype_from_name, torch_default_dtype
 
 
 @pytest.fixture(scope="class", params=[0, 1, 2])
-def model(float_tolerance, request):
+def model(model_dtype, float_tolerance, request):
     zero_species = request.param
     shifts = [3, 5, 7]
     shifts[zero_species] = 0
@@ -25,30 +27,37 @@ def model(float_tolerance, request):
         total_shift=1.0,
         shifts=shifts,
     )
-    return SequentialGraphNetwork.from_parameters(
-        shared_params=params,
-        layers={
-            "one_hot": OneHotAtomEncoding,
-            "linear": (
-                AtomwiseLinear,
-                dict(irreps_out="1x0e", out_field="e"),
+
+    with torch_default_dtype(dtype_from_name(model_dtype)):
+        model = GraphModel(
+            SequentialGraphNetwork.from_parameters(
+                shared_params=params,
+                layers={
+                    "one_hot": OneHotAtomEncoding,
+                    "linear": (
+                        AtomwiseLinear,
+                        dict(irreps_out="1x0e", out_field="e"),
+                    ),
+                    "shift": (
+                        PerSpeciesScaleShift,
+                        dict(
+                            field="e",
+                            out_field="shifted",
+                            scales=1.0,
+                            shifts=0.0,
+                            arguments_in_dataset_units=False,
+                            default_dtype=torch.get_default_dtype(),
+                        ),
+                    ),
+                    "sum": (
+                        AtomwiseReduce,
+                        dict(reduce="sum", field="shifted", out_field="sum"),
+                    ),
+                },
             ),
-            "shift": (
-                PerSpeciesScaleShift,
-                dict(
-                    field="e",
-                    out_field="shifted",
-                    scales=1.0,
-                    shifts=0.0,
-                    arguments_in_dataset_units=False,
-                ),
-            ),
-            "sum": (
-                AtomwiseReduce,
-                dict(reduce="sum", field="shifted", out_field="sum"),
-            ),
-        },
-    )
+            model_dtype=dtype_from_name(model_dtype),
+        )
+    return model
 
 
 @pytest.fixture(scope="class")
