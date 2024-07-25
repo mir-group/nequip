df74a426c3b46e944cea8e4f944d76f4341f9cfc
cw-tan <chuinwei.tan@gmail.com>
2024-07-24 23:56:55 -0400

    update nn unittests

--- tests/unit/nn/test_embed.py
+++ tests/unit/nn/test_embed.py
@@ -8,37 +8,38 @@ from nequip.nn.embedding import (
     SphericalHarmonicEdgeAttrs,
     RadialBasisEdgeEncoding,
 )
+from nequip.utils import dtype_from_name, torch_default_dtype
 
 
-def test_onehot(CH3CHO):
+def test_onehot(model_dtype, CH3CHO):
     _, data = CH3CHO
-    oh = OneHotAtomEncoding(
-        num_types=3,
-        irreps_in=data.irreps,
-    )
+    with torch_default_dtype(dtype_from_name(model_dtype)):
+        oh = OneHotAtomEncoding(
+            type_names=["A", "B", "C"],
+        )
     assert_auto_jitable(oh)
     assert_AtomicData_equivariant(oh, data)
 
 
-def test_spharm(CH3CHO):
+def test_spharm(model_dtype, CH3CHO):
     _, data = CH3CHO
-    sph = SphericalHarmonicEdgeAttrs(
-        irreps_edge_sh="0e + 1o + 2e", irreps_in=data.irreps
-    )
+    with torch_default_dtype(dtype_from_name(model_dtype)):
+        sph = SphericalHarmonicEdgeAttrs(irreps_edge_sh="0e + 1o + 2e")
     assert_auto_jitable(sph)
     assert_AtomicData_equivariant(sph, data)
 
 
-def test_radial_basis(CH3CHO):
+def test_radial_basis(model_dtype, CH3CHO):
     _, data = CH3CHO
-    basis = BesselBasis
-    cutoff = PolynomialCutoff
-    rad = RadialBasisEdgeEncoding(
-        basis,
-        cutoff,
-        basis_kwargs={"r_max": 5.0},
-        cutoff_kwargs={"r_max": 5.0},
-        irreps_in=data.irreps,
-    )
+
+    with torch_default_dtype(dtype_from_name(model_dtype)):
+        basis = BesselBasis
+        cutoff = PolynomialCutoff
+        rad = RadialBasisEdgeEncoding(
+            basis,
+            cutoff,
+            basis_kwargs={"r_max": 5.0},
+            cutoff_kwargs={"r_max": 5.0},
+        )
     assert_auto_jitable(rad)
     assert_AtomicData_equivariant(rad, data)
--- tests/unit/nn/test_rescale.py
+++ tests/unit/nn/test_rescale.py
@@ -1,33 +1,21 @@
 import pytest
 
-import sys
-
-if sys.version_info[1] >= 7:
-    import contextlib
-else:
-    # has backport of nullcontext
-    import contextlib2 as contextlib
-
 import torch
 
 from e3nn.util.test import assert_auto_jitable
 
 from nequip.nn import RescaleOutput, GraphModel
 from nequip.nn.embedding import OneHotAtomEncoding
-from nequip.data import AtomicDataDict, AtomicData
+from nequip.data import AtomicDataDict
 from nequip.utils import dtype_from_name, torch_default_dtype
 from nequip.utils.test import assert_AtomicData_equivariant
 
 
 @pytest.mark.parametrize("scale_by", [0.77, 1.0, None])
-@pytest.mark.parametrize("shift_by", [0.0, 0.4443, None])
-@pytest.mark.parametrize("scale_trainable", [True, False])
 @pytest.mark.parametrize("shift_trainable", [True, False])
 def test_rescale(
     CH3CHO,
     scale_by,
-    shift_by,
-    scale_trainable,
     shift_trainable,
     model_dtype,
 ):
@@ -35,73 +23,39 @@ def test_rescale(
     with torch_default_dtype(dtype_from_name(model_dtype)):
         oh = GraphModel(
             OneHotAtomEncoding(
-                num_types=3,
-                irreps_in=data.irreps,
+                type_names=["A", "B", "C"],
             ),
             model_dtype=dtype_from_name(model_dtype),
+            type_names=["A", "B", "C"],
         )
 
-    # some combinations are illegal and should raise
-    build_with = contextlib.nullcontext()
-    if scale_by is None and scale_trainable:
-        build_with = pytest.raises(ValueError)
-    elif shift_by is None and shift_trainable:
-        build_with = pytest.raises(ValueError)
-
-    rescale = None
-    with build_with:
-        rescale = RescaleOutput(
-            model=oh,
-            scale_keys=AtomicDataDict.NODE_ATTRS_KEY,
-            shift_keys=AtomicDataDict.NODE_ATTRS_KEY,
-            scale_by=scale_by,
-            shift_by=shift_by,
-            scale_trainable=scale_trainable,
-            shift_trainable=shift_trainable,
-        )
-
-    if rescale is None:
-        return
+    rescale = RescaleOutput(
+        model=oh,
+        scale_keys=AtomicDataDict.NODE_ATTRS_KEY,
+        scale_by=scale_by,
+    )
 
     # == Check basics ==
     assert_auto_jitable(rescale)
     for training_mode in [True, False]:
         rescale.train(training_mode)
-        rescale(AtomicData.to_AtomicDataDict(data))
+        rescale(data)
         assert_AtomicData_equivariant(rescale, data)
 
     # == Check scale/shift ==
-    for training_mode in [True, False]:
-        rescale.train(training_mode)
+    # oh_out is in model_dtype
+    oh_out = oh(data)[AtomicDataDict.NODE_ATTRS_KEY]
+    # rescale_out is in default_dtype
+    rescale_out = rescale(data)[AtomicDataDict.NODE_ATTRS_KEY]
 
-        # oh_out is in model_dtype
-        oh_out = oh(AtomicData.to_AtomicDataDict(data))[AtomicDataDict.NODE_ATTRS_KEY]
-        # rescale_out is in default_dtype
-        rescale_out = rescale(AtomicData.to_AtomicDataDict(data))[
-            AtomicDataDict.NODE_ATTRS_KEY
-        ]
+    # only default_dtype=float64 will be tested (see above pytest.skip check)
+    # model_dtype can be float32 or float64
+    # so we cast oh_out (model_dtype) to rescale_out (model_dtype) for testing
+    oh_out = oh_out.to(dtype=rescale_out.dtype)
 
-        # only default_dtype=float64 will be tested (see above pytest.skip check)
-        # model_dtype can be float32 or float64
-        # so we cast oh_out (model_dtype) to rescale_out (model_dtype) for testing
-        oh_out = oh_out.to(dtype=rescale_out.dtype)
-
-        if training_mode:
-            assert torch.all(oh_out == rescale_out)
-            continue  # don't test anything else
-        # we are now in eval mode if here, test rescaling
-        # node attrs are a one hot, so we know orig then are zeros and ones
-        if scale_by is None and shift_by is None:
-            assert torch.all(oh_out == rescale_out)
-        if shift_by is None:
-            # no shift preserves zeros
-            assert torch.all((rescale_out == 0.0) == (oh_out == 0.0))
-        if scale_by is None and shift_by is not None:
-            # check that difference is right
-            assert torch.allclose(rescale_out - oh_out, torch.as_tensor(shift_by))
-        if scale_by is not None and shift_by is None:
-            # check that ratio is right
-            ratio = torch.nan_to_num(rescale_out / oh_out)
-            assert torch.allclose(ratio[oh_out != 0.0], torch.as_tensor(scale_by))
-        if scale_by is not None and shift_by is not None:
-            assert torch.allclose(rescale_out, scale_by * oh_out + shift_by)
+    # node attrs are a one hot, so we know orig then are zeros and ones
+    if scale_by is None:
+        assert torch.all(oh_out == rescale_out)
+    else:
+        ratio = torch.nan_to_num(rescale_out / oh_out)
+        assert torch.allclose(ratio[oh_out != 0.0], torch.as_tensor(scale_by))
