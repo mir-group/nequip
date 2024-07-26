27916c185bd11e13845be12b59d00dd3d809a143
cw-tan <chuinwei.tan@gmail.com>
2024-07-26 16:42:09 -0400

    rework EMTTestDataset

--- nequip/data/_dataset/_test_data.py
+++ nequip/data/_dataset/_test_data.py
@@ -1,7 +1,8 @@
-from typing import Optional, List, Dict, Any, Tuple
+from typing import Union, Callable, List, Tuple
 import copy
 
 import numpy as np
+import torch
 
 import ase
 import ase.build
@@ -9,7 +10,6 @@ from ase.calculators.emt import EMT
 
 from .. import AtomicDataDict
 from ._base_datasets import AtomicDataset
-from ..transforms import TypeMapper
 
 
 class EMTTestDataset(AtomicDataset):
@@ -23,19 +23,14 @@ class EMTTestDataset(AtomicDataset):
 
     def __init__(
         self,
-        root: str,
+        transforms: List[Callable] = [],
         supercell: Tuple[int, int, int] = (4, 4, 4),
         sigma: float = 0.1,
         element: str = "Cu",
         num_frames: int = 10,
         dataset_seed: int = 123456,
-        file_name: Optional[str] = None,
-        url: Optional[str] = None,
-        AtomicData_options: Dict[str, Any] = {},
-        include_frames: Optional[List[int]] = None,
-        type_mapper: TypeMapper = None,
     ):
-        # Set properties for hashing
+        super().__init__(transforms=transforms)
         assert element in ("Cu", "Pd", "Au", "Pt", "Al", "Ni", "Ag")
         self.element = element
         self.sigma = sigma
@@ -43,42 +38,39 @@ class EMTTestDataset(AtomicDataset):
         self.num_frames = num_frames
         self.dataset_seed = dataset_seed
 
-        super().__init__(
-            file_name=file_name,
-            url=url,
-            root=root,
-            AtomicData_options=AtomicData_options,
-            include_frames=include_frames,
-            type_mapper=type_mapper,
-        )
-
-    @property
-    def raw_file_names(self):
-        return []
-
-    @property
-    def raw_dir(self):
-        return "raw"
-
-    def get_data(self):
-        rng = np.random.default_rng(self.dataset_seed)
+        # generate data
         base_atoms = ase.build.bulk(self.element, "fcc").repeat(self.supercell)
         base_atoms.calc = EMT()
         orig_pos = copy.deepcopy(base_atoms.positions)
-        datas = []
-        for _ in range(self.num_frames):
+        rng = np.random.default_rng(self.dataset_seed)
+        self.data_list = []
+        for idx in range(len(self)):
             base_atoms.positions[:] = orig_pos
             base_atoms.positions += rng.normal(
                 loc=0.0, scale=self.sigma, size=base_atoms.positions.shape
             )
-
-            datas.append(
-                AtomicDataDict.from_ase(
-                    base_atoms.copy(),
-                    forces=base_atoms.get_forces(),
-                    total_energy=base_atoms.get_potential_energy(),
-                    stress=base_atoms.get_stress(voigt=False),
-                    **self.AtomicData_options
+            self.data_list.append(
+                AtomicDataDict.from_dict(
+                    {
+                        "pos": base_atoms.positions,
+                        "cell": np.array(base_atoms.get_cell()),
+                        "pbc": base_atoms.get_pbc(),
+                        "atomic_numbers": base_atoms.get_atomic_numbers(),
+                        "forces": base_atoms.get_forces(),
+                        "total_energy": base_atoms.get_potential_energy(),
+                        "stress": base_atoms.get_stress(voigt=True),
+                    }
                 )
             )
-        return datas
+
+    def __len__(self) -> int:
+        return self.num_frames
+
+    def get_data_list(
+        self,
+        indices: Union[List[int], torch.Tensor, slice],
+    ) -> List[AtomicDataDict.Type]:
+        if isinstance(indices, slice):
+            return self.data_list[indices]
+        else:
+            return [self.data_list[index] for index in indices]
