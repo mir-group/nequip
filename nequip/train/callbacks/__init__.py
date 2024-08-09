7f00211363072c8173ddfb19071b2f3ff07b66a0
cw-tan <chuinwei.tan@gmail.com>
2024-08-09 11:59:13 -0400

    add ExponentialMovingAverage callback from NeMo

--- nequip/train/callbacks/__init__.py
+++ nequip/train/callbacks/__init__.py
@@ -1,5 +1,11 @@
 from .softadapt import SoftAdapt
 from .loss_coeff_scheduler import LossCoefficientScheduler
 from .loss_coeff_monitor import LossCoefficientMonitor
+from .nemo_ema import NeMoExponentialMovingAverage
 
-__all__ = [SoftAdapt, LossCoefficientScheduler, LossCoefficientMonitor]
+__all__ = [
+    SoftAdapt,
+    LossCoefficientScheduler,
+    LossCoefficientMonitor,
+    NeMoExponentialMovingAverage,
+]
