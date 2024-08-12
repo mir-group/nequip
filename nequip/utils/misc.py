2a106304c1c902a7c989583e001be4c88c47d564
cw-tan <chuinwei.tan@gmail.com>
2024-08-12 14:41:51 -0400

    re-introduce type str formatter for logging in utils

--- nequip/utils/misc.py
+++ nequip/utils/misc.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union, List
 import contextlib
 
 import torch
@@ -32,3 +32,29 @@ def torch_default_dtype(dtype):
         yield
     finally:
         torch.set_default_dtype(orig_default_dtype)
+
+
+def format_type_vals(
+    data: list, type_names: List[str], element_formatter: str = ".6f"
+) -> str:
+    data = torch.as_tensor(data) if data is not None else None
+
+    if data.numel() == 1:
+        data = torch.tensor(data.item())
+
+    if data is None:
+        return f"[{', '.join(type_names)}: None]"
+    elif data.ndim == 0:
+        return (f"[{', '.join(type_names)}: {{:{element_formatter}}}]").format(data)
+    elif data.ndim == 1 and len(data) == len(type_names):
+        return (
+            "["
+            + ", ".join(
+                f"{{{i}[0]}}: {{{i}[1]:{element_formatter}}}" for i in range(len(data))
+            )
+            + "]"
+        ).format(*zip(type_names, data))
+    else:
+        raise ValueError(
+            f"Don't know how to format data=`{data}` for types {type_names} with element_formatter=`{element_formatter}`"
+        )
