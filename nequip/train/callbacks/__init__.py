a004f4f17a4ed584bca0035923f9074818fb7cb1
cw-tan <chuinwei.tan@gmail.com>
2024-08-19 09:39:33 -0400

    introduce TestTimeXYZFileWriter callback for testing

--- nequip/train/callbacks/__init__.py
+++ nequip/train/callbacks/__init__.py
@@ -2,10 +2,12 @@ from .softadapt import SoftAdapt
 from .loss_coeff_scheduler import LossCoefficientScheduler
 from .loss_coeff_monitor import LossCoefficientMonitor
 from .nemo_ema import NeMoExponentialMovingAverage
+from .write_xyz import TestTimeXYZFileWriter
 
 __all__ = [
     SoftAdapt,
     LossCoefficientScheduler,
     LossCoefficientMonitor,
     NeMoExponentialMovingAverage,
+    TestTimeXYZFileWriter,
 ]
