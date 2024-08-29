0ae5d59870651a53808d83b579ecd5e85a052e16
cw-tan <chuinwei.tan@gmail.com>
2024-08-29 18:51:45 -0400

    introduce sGDMLNPZDataset

--- nequip/data/dataset/__init__.py
+++ nequip/data/dataset/__init__.py
@@ -1,6 +1,14 @@
 from ._base_datasets import AtomicDataset
 from ._ase_dataset import ASEDataset
+from ._sgdml_npz_dataset import sGDMLNPZDataset
 from ._hdf5_dataset import HDF5Dataset
 from ._test_data import EMTTestDataset
 
-__all__ = [AtomicDataset, ASEDataset, HDF5Dataset, EMTTestDataset]
+
+__all__ = [
+    AtomicDataset,
+    ASEDataset,
+    sGDMLNPZDataset,
+    HDF5Dataset,
+    EMTTestDataset,
+]
