1da4248c87c9452c7c8f3d8591746b0761de42c0
cw-tan <chuinwei.tan@gmail.com>
2024-09-26 20:00:11 -0400

    clean ase dataset

--- nequip/data/dataset/_ase_dataset.py
+++ nequip/data/dataset/_ase_dataset.py
@@ -1,5 +1,3 @@
-from typing import Union, Dict, List, Optional, Callable, Any
-
 import torch
 import ase
 import ase.io
@@ -7,83 +5,38 @@ import ase.io
 from .. import AtomicDataDict
 from ._base_datasets import AtomicDataset
 
+from typing import Union, Dict, List, Optional, Callable, Any
+
+# TODO: link "standard keys" under `include_keys` to docs
+
 
 class ASEDataset(AtomicDataset):
     r"""``AtomicDataset`` for `ASE <https://wiki.fysik.dtu.dk/ase/ase/io/io.html>`_-readable file formats.
 
-    Example: Given an atomic data stored in ``H2.extxyz`` that looks like below:
-
-    ::
-
-      2
-      Properties=species:S:1:pos:R:3 energy=-10 user_label=2.0 pbc="F F F"
-       H       0.00000000       0.00000000       0.00000000
-       H       0.00000000       0.00000000       1.02000000
-
-    An example of the config would is as follows.
-
-    ::
-
-      _target_: nequip.data.dataset.ASEDataset
-      file_name: H2.extxyz
-      transforms:
-        - _target_: nequip.data.transforms.NeighborListTransform
-          r_max: 4.0
-        - _target_: nequip.data.transforms.ChemicalSpeciesToAtomTypeMapper
-          chemical_symbols: [H]
-      ase_args:
-        format: extxyz
-      include_keys:
-        - user_label
-      key_mapping:
-          user_label: label0
-
-    Any ASE-parsable file format works. For VASP files, the config could be
-
-    ::
-
-      _target_: nequip.data.dataset.ASEDataset
-      file_name: OUTCAR
-      transforms:
-      - _target_: nequip.data.transforms.NeighborListTransform
-        r_max: 4.0
-      - _target_: nequip.data.transforms.ChemicalSpeciesToAtomTypeMapper
-        chemical_symbols: [H]
-      ase_args:
-        format: vasp-out
-      key_mapping:
-        free_energy: total_energy
-
     Args:
-        file_name (str): path to ASE-readable file
+        file_path (str): path to ASE-readable file
         transforms (List[Callable]): list of data transforms
         ase_args (Dict[str, Any]): arguments for ``ase.io.iread`` (see `here <https://wiki.fysik.dtu.dk/ase/ase/io/io.html#ase.io.iread>`_)
-        include_keys (List[str]): the keys that needs to be parsed into dataset in addition to forces and energy.
-             The data stored in ``ase.atoms.Atoms.array`` has the lowest priority,
-             and it will be overrided by data in ``ase.atoms.Atoms.info`` and ``ase.atoms.Atoms.calc.results``
+        include_keys (List[str]): the keys that needs to be parsed into dataset in addition to standard keys. The data stored in ``ase.atoms.Atoms.array`` has the lowest priority, and it will be overrided by data in ``ase.atoms.Atoms.info`` and ``ase.atoms.Atoms.calc.results``
         key_mapping (Dict[str, str]): mapping of ``ase`` keys to ``AtomicDataDict`` keys
-
     """
-
-    file_name: str
-    ase_args: Dict[str, Any]
 
     def __init__(
         self,
-        file_name: str,
+        file_path: str,
         transforms: List[Callable] = [],
-        ase_args: dict = {},
+        ase_args: Dict[str, Any] = {},
         include_keys: Optional[list] = [],
         key_mapping: Optional[Dict[str, str]] = {},
     ):
         super().__init__(transforms=transforms)
-        self.file_name = file_name
+        self.file_path = file_path
         # process ase_args
         self.ase_args = {}
         self.ase_args.update(ase_args)
         assert "index" not in self.ase_args
         assert "filename" not in self.ase_args
-        self.ase_args.update({"filename": file_name})
+        self.ase_args.update({"filename": self.file_path})
 
         # read file and construct list of AtomicDataDicts
         self.data_list: List[AtomicDataDict.Type] = []
