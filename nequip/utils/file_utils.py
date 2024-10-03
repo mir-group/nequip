7f1896cad89b96f51f2d833b57e3fcb3c57583ab
cw-tan <chuinwei.tan@gmail.com>
2024-10-03 11:15:28 -0400

    use RankedLogger for autodownload utils and minor clean up

--- nequip/utils/file_utils.py
+++ nequip/utils/file_utils.py
@@ -5,18 +5,19 @@ import os
 import os.path as osp
 import urllib
 import zipfile
-import logging
+from typing import Optional
+from .logger import RankedLogger
 
-logger = logging.getLogger(__name__)
+logger = RankedLogger(__name__, rank_zero_only=True)
 
 
-def download_url(url, folder, filename=None):
-    r"""Downloads the content of an URL to a specific folder.
+def download_url(url: str, folder: str, filename: Optional[str] = None) -> None:
+    """Downloads the content of an URL to a specific folder.
 
     Args:
-        url (str): The url.
-        folder (str): The folder.
-        filename (str): The filename
+        url (str)     : the url
+        folder (str)  : the folder
+        filename (str): the filename
     """
 
     filename = url.rpartition("/")[2].split("?")[0] if filename is None else filename
@@ -24,23 +25,21 @@ def download_url(url, folder, filename=None):
     if osp.exists(path):  # pragma: no cover
         logger.info(f"Using existing file {path}")
         return path
-
     logger.info(f"Downloading from {url}")
     os.makedirs(folder, exist_ok=True)
     context = ssl._create_unverified_context()
     data = urllib.request.urlopen(url, context=context)
     with open(path, "wb") as f:
         f.write(data.read())
-
     return path
 
 
-def extract_zip(path, folder):
-    r"""Extracts a zip archive to a specific folder.
+def extract_zip(path: str, folder: str) -> None:
+    """Extracts a zip archive to a specific folder.
 
     Args:
-        path (str): The path to the tar archive.
-        folder (str): The folder.
+        path (str)  : the path to the tar archive
+        folder (str): the folder
     """
     with zipfile.ZipFile(path, "r") as f:
         f.extractall(folder)
