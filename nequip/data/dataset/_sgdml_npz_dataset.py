036c3837a9993cfc09e963bb1a762741e3ac86c7
cw-tan <chuinwei.tan@gmail.com>
2024-10-05 01:18:48 -0400

    sGDMLNPZDataset: always in memory -> load from disk for batches

--- nequip/data/dataset/_sgdml_npz_dataset.py
+++ nequip/data/dataset/_sgdml_npz_dataset.py
@@ -10,6 +10,8 @@ from typing import Union, Dict, List, Callable
 class sGDMLNPZDataset(AtomicDataset):
     """``AtomicDataset`` that loads data from an NPZ file following sGDML conventions.
 
+    The ``sGDMLNPZDataset`` avoids loading the whole dataset into memory.
+
     Args:
         file_path (str): path to npz file
         transforms (List[Callable]): list of data transforms
@@ -29,38 +31,40 @@ class sGDMLNPZDataset(AtomicDataset):
     ):
         super().__init__(transforms=transforms)
         self.file_path = file_path
-
-        # TODO: need to check that key mapping is unique?
-        pos_key = None
-        for k, v in key_mapping.items():
-            if v == AtomicDataDict.POSITIONS_KEY:
-                pos_key = k
-        assert pos_key is not None, "No key indicating position found in npz dataset"
-
         self.key_mapping = key_mapping
-        npz_data = np.load(self.file_path)
-        num_frames = npz_data[pos_key].shape[0]
 
-        # set up list of AtomicDataDicts
-        self.data_list = []
-        for idx in range(num_frames):
-            data_dict = {}
-            for k, v in self.key_mapping.items():
-                # special case for now, may generalize if needed in the future
-                if v == AtomicDataDict.ATOMIC_NUMBERS_KEY:
-                    data_dict[v] = npz_data[k]
-                else:
-                    data_dict[v] = npz_data[k][idx]
-            self.data_list.append(AtomicDataDict.from_dict(data_dict))
+        # use energy array to get num_frames (small to load into memory)
+        E_key = None
+        for k, v in key_mapping.items():
+            if v == AtomicDataDict.TOTAL_ENERGY_KEY:
+                E_key = k
+        assert (
+            E_key is not None
+        ), "No key corresponding to `total_energy` found in npz dataset"
+        with np.load(self.file_path, mmap_mode="r") as npz_data:
+            self.num_frames = npz_data[E_key].shape[0]
 
     def __len__(self) -> int:
-        return len(self.data_list)
+        return self.num_frames
 
     def get_data_list(
         self,
         indices: Union[List[int], torch.Tensor, slice],
     ) -> List[AtomicDataDict.Type]:
+
         if isinstance(indices, slice):
-            return self.data_list[indices]
-        else:
-            return [self.data_list[index] for index in indices]
+            indices = list(range(*indices.indices(self.num_frames)))
+
+        # memory-map the file
+        with np.load(self.file_path, mmap_mode="r") as npz_data:
+            data_list = []
+            for idx in indices:
+                data_dict = {}
+                for k, v in self.key_mapping.items():
+                    # special case for now, may generalize if needed in the future
+                    if v == AtomicDataDict.ATOMIC_NUMBERS_KEY:
+                        data_dict[v] = npz_data[k]
+                    else:
+                        data_dict[v] = npz_data[k][idx]
+                data_list.append(AtomicDataDict.from_dict(data_dict))
+            return data_list
