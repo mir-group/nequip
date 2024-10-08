37b25a514ca9c916b7ae2cb65807c7c30b6a3a60
Linux-cpp-lisp <1473644+Linux-cpp-lisp@users.noreply.github.com>
2024-10-08 18:38:33 -0400

    remove unnecessary @torch.jit.script's

--- nequip/nn/nonlinearities.py
+++ nequip/nn/nonlinearities.py
@@ -2,7 +2,10 @@ import torch
 
 import math
 
+# Technically, use of this module should probably be guarded by conditional_torchscript_jit
+# But its use as a drop-in replacement for functions like torch.nn.functional.silu makes that
+# difficult, so, given the rarety of its use, we have just removed @torch.jit.script
 
-@torch.jit.script
+
 def ShiftedSoftPlus(x):
     return torch.nn.functional.softplus(x) - math.log(2.0)
