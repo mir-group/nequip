cffb6805a9aecd9952e872840cf80944b94c23ef
cw-tan <chuinwei.tan@gmail.com>
2024-10-09 23:06:55 -0400

    remove add_to_config and add logging in versions util

--- nequip/utils/versions.py
+++ nequip/utils/versions.py
@@ -4,14 +4,11 @@ import torch
 import e3nn
 import nequip
 
-from omegaconf import open_dict
 from .git import get_commit
-
+from .logger import RankedLogger
 from typing import Tuple, Final
-import logging
 
-logger = logging.getLogger(__name__)
-
+logger = RankedLogger(__name__, rank_zero_only=True)
 
 _TORCH_IS_GE_1_13: Final[bool] = packaging.version.parse(
     torch.__version__
@@ -50,10 +47,15 @@ def get_current_code_versions(config) -> Tuple[dict, dict]:
             code_commits.add(builder[0])
     code_commits = {code: get_commit(code) for code in code_commits}
     code_commits = {k: v for k, v in code_commits.items() if v is not None}
+
+    logger.info("{:^29}".format("Version Information"))
+    for k, v in code_versions.items():
+        logger.info(f"{k:^14}:{v:^14}")
+
     return code_versions, code_commits
 
 
-def check_code_version(config, add_to_config: bool = False):
+def check_code_version(config):
     current_code_versions, current_code_commits = get_current_code_versions(config)
     code_versions, code_commits = get_config_code_versions(config)
 
@@ -75,8 +77,4 @@ def check_code_version(config, add_to_config: bool = False):
                 f"vs  original commit: {commit}"
             )
 
-    if add_to_config:
-        with open_dict(config):
-            for code, version in current_code_versions.items():
-                config[f"{code}_version"] = str(version)
-            config[CODE_COMMITS_KEY] = current_code_commits
+    return current_code_versions, code_commits
