269311a752aa741d5ffad6c160f82a4e3a8d2368
cw-tan <chuinwei.tan@gmail.com>
2024-10-16 22:19:09 -0400

    update data stats unittests to account for non-torch.Tensor stats manager outputs

--- tests/unit/data/test_statistics.py
+++ tests/unit/data/test_statistics.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 from nequip.data import AtomicDataDict
 from nequip.data import (
@@ -86,41 +87,43 @@ class TestStatistics:
 
         # test energy
         eng = batch[AtomicDataDict.TOTAL_ENERGY_KEY]
-        assert torch.allclose(stats_dict["E_mean"], torch.mean(eng))
-        assert torch.allclose(stats_dict["E_std"], torch.std(eng))
+        assert np.allclose(stats_dict["E_mean"], torch.mean(eng).item())
+        assert np.allclose(stats_dict["E_std"], torch.std(eng).item())
         eng_per_atom = eng.reshape(-1) / batch[AtomicDataDict.NUM_NODES_KEY]
-        assert torch.allclose(stats_dict["per_atom_E_mean"], torch.mean(eng_per_atom))
-        assert torch.allclose(stats_dict["per_atom_E_std"], torch.std(eng_per_atom))
+        assert np.allclose(
+            stats_dict["per_atom_E_mean"], torch.mean(eng_per_atom).item()
+        )
+        assert np.allclose(stats_dict["per_atom_E_std"], torch.std(eng_per_atom).item())
 
         # test force
         f_raveled = batch[AtomicDataDict.FORCE_KEY].flatten()
-        assert torch.allclose(stats_dict["F_mean"], torch.mean(f_raveled))
-        assert torch.allclose(stats_dict["F_std"], torch.std(f_raveled))
-        assert torch.allclose(
-            stats_dict["F_rms"], torch.sqrt(torch.mean(f_raveled.square()))
+        assert np.allclose(stats_dict["F_mean"], torch.mean(f_raveled).item())
+        assert np.allclose(stats_dict["F_std"], torch.std(f_raveled).item())
+        assert np.allclose(
+            stats_dict["F_rms"], torch.sqrt(torch.mean(f_raveled.square())).item()
         )
-        assert torch.allclose(stats_dict["F_absmax"], f_raveled.abs().max())
+        assert np.allclose(stats_dict["F_absmax"], f_raveled.abs().max().item())
 
         # test num atoms, num neighbors
         expected_avg_num_atoms = torch.mean(
             batch[AtomicDataDict.NUM_NODES_KEY].to(torch.get_default_dtype())
         )
-        assert torch.allclose(stats_dict["num_atoms_mean"], expected_avg_num_atoms)
+        assert np.allclose(stats_dict["num_atoms_mean"], expected_avg_num_atoms.item())
 
         expected_ave_num_neighbors = torch.mean(
             torch.unique(batch[AtomicDataDict.EDGE_INDEX_KEY][0], return_counts=True)[
                 1
             ].to(torch.get_default_dtype())
         )
-        assert torch.allclose(
-            stats_dict["num_neighbors_mean"], expected_ave_num_neighbors
+        assert np.allclose(
+            stats_dict["num_neighbors_mean"], expected_ave_num_neighbors.item()
         )
 
         data = AtomicDataDict.with_edge_vectors(batch, with_lengths=True)
         lengths = data[AtomicDataDict.EDGE_LENGTH_KEY]
-        assert torch.allclose(stats_dict["edge_lengths_mean"], torch.mean(lengths))
-        assert torch.allclose(stats_dict["edge_lengths_std"], torch.std(lengths))
-        assert torch.allclose(stats_dict["edge_lengths_min"], lengths.min())
+        assert np.allclose(stats_dict["edge_lengths_mean"], torch.mean(lengths).item())
+        assert np.allclose(stats_dict["edge_lengths_std"], torch.std(lengths).item())
+        assert np.allclose(stats_dict["edge_lengths_min"], lengths.min().item())
 
     @pytest.mark.parametrize("num_trainval_test", [(200, 100), (500, 200)])
     @pytest.mark.parametrize("batch_size", [1, 2, 5, 50])
@@ -160,15 +163,15 @@ class TestStatistics:
         atom_types = batch[AtomicDataDict.ATOM_TYPE_KEY]
         C_forces = batch[AtomicDataDict.FORCE_KEY][atom_types == 0]
         H_forces = batch[AtomicDataDict.FORCE_KEY][atom_types == 1]
-        assert torch.allclose(stats_dict["F_mean_C"], torch.mean(C_forces))
-        assert torch.allclose(stats_dict["F_mean_H"], torch.mean(H_forces))
-        assert torch.allclose(stats_dict["F_std_C"], torch.std(C_forces))
-        assert torch.allclose(stats_dict["F_std_H"], torch.std(H_forces))
-        assert torch.allclose(
-            stats_dict["F_rms_C"], torch.sqrt(torch.mean(C_forces.square()))
+        assert np.allclose(stats_dict["F_mean_C"], torch.mean(C_forces).item())
+        assert np.allclose(stats_dict["F_mean_H"], torch.mean(H_forces).item())
+        assert np.allclose(stats_dict["F_std_C"], torch.std(C_forces).item())
+        assert np.allclose(stats_dict["F_std_H"], torch.std(H_forces).item())
+        assert np.allclose(
+            stats_dict["F_rms_C"], torch.sqrt(torch.mean(C_forces.square())).item()
         )
-        assert torch.allclose(
-            stats_dict["F_rms_H"], torch.sqrt(torch.mean(H_forces.square()))
+        assert np.allclose(
+            stats_dict["F_rms_H"], torch.sqrt(torch.mean(H_forces.square())).item()
         )
 
         _, count = torch.unique(batch[AtomicDataDict.ATOM_TYPE_KEY], return_counts=True)
@@ -187,15 +190,15 @@ class TestStatistics:
         CH_type = torch.logical_and(edge_type[0] == 0, edge_type[1] == 1)
         HC_type = torch.logical_and(edge_type[0] == 1, edge_type[1] == 0)
         HH_type = torch.logical_and(edge_type[0] == 1, edge_type[1] == 1)
-        assert torch.allclose(
-            stats_dict["edge_lengths_std_CC"], torch.std(lengths[CC_type])
+        assert np.allclose(
+            stats_dict["edge_lengths_std_CC"], torch.std(lengths[CC_type]).item()
         )
-        assert torch.allclose(
-            stats_dict["edge_lengths_std_CH"], torch.std(lengths[CH_type])
+        assert np.allclose(
+            stats_dict["edge_lengths_std_CH"], torch.std(lengths[CH_type]).item()
         )
-        assert torch.allclose(
-            stats_dict["edge_lengths_std_HC"], torch.std(lengths[HC_type])
+        assert np.allclose(
+            stats_dict["edge_lengths_std_HC"], torch.std(lengths[HC_type]).item()
         )
-        assert torch.allclose(
-            stats_dict["edge_lengths_std_HH"], torch.std(lengths[HH_type])
+        assert np.allclose(
+            stats_dict["edge_lengths_std_HH"], torch.std(lengths[HH_type]).item()
         )
