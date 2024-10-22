cb437ba06aef54c130d53e78534672b1b02d6123
cw-tan <chuinwei.tan@gmail.com>
2024-10-21 21:36:06 -0400

    move `tp_path_exists` from `utils` to `nn`

--- nequip/nn/_util.py
+++ nequip/nn/_util.py
@@ -1,5 +1,7 @@
 import torch
 
+from e3nn import o3
+
 from nequip.data import AtomicDataDict
 from nequip.nn import GraphModuleMixin
 
@@ -70,3 +72,15 @@ def scatter(
         return out.scatter_add_(dim, index, src)
     else:
         return out.scatter_add_(dim, index, src)
+
+
+def tp_path_exists(irreps_in1, irreps_in2, ir_out):
+    irreps_in1 = o3.Irreps(irreps_in1).simplify()
+    irreps_in2 = o3.Irreps(irreps_in2).simplify()
+    ir_out = o3.Irrep(ir_out)
+
+    for _, ir1 in irreps_in1:
+        for _, ir2 in irreps_in2:
+            if ir_out in ir1 * ir2:
+                return True
+    return False
