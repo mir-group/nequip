cf3903e716f291e5825e29f3a14e2a896574a50c
cw-tan <chuinwei.tan@gmail.com>
2024-10-23 23:41:57 -0400

    add `VirialToStressTransform` and associated unittest

--- nequip/data/transforms/__init__.py
+++ nequip/data/transforms/__init__.py
@@ -1,7 +1,9 @@
 from .type_mapper import ChemicalSpeciesToAtomTypeMapper
 from .neighborlist import NeighborListTransform
+from .virial_to_stress import VirialToStressTransform
 
 __all__ = [
     ChemicalSpeciesToAtomTypeMapper,
     NeighborListTransform,
+    VirialToStressTransform,
 ]
