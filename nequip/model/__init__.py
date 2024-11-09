d98278f283693331ff053afb448627ce30e2fcc6
cw-tan <chuinwei.tan@gmail.com>
2024-11-09 02:17:03 -0500

    refactor ZBL pair potential model builder and fix associated tests
    Co-authored-by: Alby M. <1473644+Linux-cpp-lisp@users.noreply.github.com>

--- nequip/model/__init__.py
+++ nequip/model/__init__.py
@@ -1,14 +1,12 @@
 from ._weight_init import uniform_initialize_FCs
-from ._pair_potential import PairPotential, PairPotentialTerm
-
 from .utils import model_builder
 from .nequip_models import NequIPGNNModel, FullNequIPGNNModel
+from .pair_potential import ZBLPairPotential
 
 __all__ = [
     uniform_initialize_FCs,
-    PairPotential,
-    PairPotentialTerm,
     model_builder,
     NequIPGNNModel,
     FullNequIPGNNModel,
+    ZBLPairPotential,
 ]
