4a680fa1e8fb1174b143d6a7a5f69c5159f09360
cw-tan <chuinwei.tan@gmail.com>
2024-11-09 02:22:55 -0500

    remove `auto_init.py`

--- nequip/utils/__init__.py
+++ nequip/utils/__init__.py
@@ -1,8 +1,3 @@
-from .auto_init import (
-    instantiate_from_cls_name,
-    instantiate,
-    get_w_prefix,
-)
 from .config import Config
 from .modules import find_first_of_type
 from .misc import dtype_to_name, dtype_from_name, torch_default_dtype
@@ -12,9 +7,6 @@ from .compile import conditional_torchscript_mode, conditional_torchscript_jit
 from .versions import get_current_code_versions
 
 __all__ = [
-    instantiate_from_cls_name,
-    instantiate,
-    get_w_prefix,
     Config,
     find_first_of_type,
     dtype_to_name,
