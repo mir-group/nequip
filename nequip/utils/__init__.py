a99d0bad3e8373b185122f56d2a4ca7c69b557b3
cw-tan <chuinwei.tan@gmail.com>
2024-11-23 22:41:51 -0500

    remove unused `Config` utils and tests

--- nequip/utils/__init__.py
+++ nequip/utils/__init__.py
@@ -1,4 +1,3 @@
-from .config import Config
 from .modules import find_first_of_type
 from .misc import dtype_to_name, dtype_from_name, torch_default_dtype
 from .file_utils import download_url, extract_zip
@@ -7,7 +6,6 @@ from .compile import conditional_torchscript_mode, conditional_torchscript_jit
 from .versions import get_current_code_versions
 
 __all__ = [
-    Config,
     find_first_of_type,
     dtype_to_name,
     dtype_from_name,
