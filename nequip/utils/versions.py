750e818364237c85d55d73f2467f7818ae5d9710
cw-tan <chuinwei.tan@gmail.com>
2024-11-27 14:54:07 -0500

    add verbosity arg to `get_current_code_versions` utils

--- nequip/utils/versions.py
+++ nequip/utils/versions.py
@@ -15,13 +15,14 @@ for ep in _DISCOVERED_NEQUIP_EXTENSION:
     _DEFAULT_VERSION_CODES.append(importlib.import_module(ep.value))
 
 
-def get_current_code_versions() -> Tuple[dict, dict]:
+def get_current_code_versions(verbose=True) -> Tuple[dict, dict]:
     code_versions = {}
     for code in _DEFAULT_VERSION_CODES:
         code_versions[code.__name__] = str(code.__version__)
 
-    logger.info("{:^29}".format("Version Information"))
-    for k, v in code_versions.items():
-        logger.info(f"{k:^14}:{v:^14}")
+    if verbose:
+        logger.info("{:^29}".format("Version Information"))
+        for k, v in code_versions.items():
+            logger.info(f"{k:^14}:{v:^14}")
 
     return code_versions
