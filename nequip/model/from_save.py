a2a28e15141717bd6c7bed1026f6a18604a8ca4a
cw-tan <chuinwei.tan@gmail.com>
2025-01-07 00:26:49 -0500

    let `ModelFromCheckpoint` and `ModelFromPackage` optionally handle global options setting

--- nequip/model/from_save.py
+++ nequip/model/from_save.py
@@ -1,11 +1,17 @@
 import torch
 from nequip.utils import get_current_code_versions
+from nequip.utils._global_options import _set_global_options
 
+from omegaconf import OmegaConf
 import hydra
 import warnings
+from typing import Union
 
 
-def ModelFromCheckpoint(checkpoint_path: str):
+def ModelFromCheckpoint(
+    checkpoint_path: str,
+    set_global_options: Union[str, bool] = False,
+):
     """Builds model from a NequIP framework checkpoint file.
 
     There are two common use modes of this model builder.
@@ -42,6 +48,11 @@ def ModelFromCheckpoint(checkpoint_path: str):
                 f"`{code}` versions differ between the checkpoint file ({ckpt_version}) and the current run ({session_version}) -- `ModelFromCheckpoint` will be built with the current run's versions, but please check that this decision is as intended."
             )
 
+    if set_global_options:
+        global_options = checkpoint["hyper_parameters"]["info_dict"]["global_options"]
+        global_options.update({"warn_on_override": set_global_options == "warn"})
+        _set_global_options(**global_options)
+
     # === load model via lightning module ===
     training_module = hydra.utils.get_class(
         checkpoint["hyper_parameters"]["info_dict"]["training_module"]["_target_"]
@@ -50,7 +61,10 @@ def ModelFromCheckpoint(checkpoint_path: str):
     return lightning_module.model
 
 
-def ModelFromPackage(package_path: str):
+def ModelFromPackage(
+    package_path: str,
+    set_global_options: Union[str, bool] = False,
+):
     """Builds model from a NequIP framework packaged zip file constructed with ``nequip-package``.
 
     Args:
@@ -68,6 +82,17 @@ def ModelFromPackage(package_path: str):
             module="torch.package.package_importer",
         )
         imp = torch.package.PackageImporter(package_path)
+        # set global options before loading model
+        # TODO: do we need to set global options with the packaged code?
+        # for now, nothing in global options should require this, but maybe if e3nn were interned ...
+        if set_global_options:
+            config = imp.load_text(package="model", resource="config.yaml")
+            global_options = OmegaConf.to_container(OmegaConf.create(config))[
+                "global_options"
+            ]
+            global_options.update({"warn_on_override": set_global_options == "warn"})
+            _set_global_options(**global_options)
         model = imp.load_pickle(package="model", resource="model.pkl")
+
     # NOTE: model returned is not a GraphModel object tied to the `nequip` in current Python env, but a GraphModel object from the packaged zip file
     return model
