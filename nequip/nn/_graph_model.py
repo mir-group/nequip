7028afa0cb98b3efcf28ca373c2260181ba8450f
cw-tan <chuinwei.tan@gmail.com>
2025-01-06 23:23:54 -0500

    refactor model metadata utilities into `GraphModel`

--- nequip/nn/_graph_model.py
+++ nequip/nn/_graph_model.py
@@ -1,12 +1,40 @@
-from typing import List, Dict, Any, Optional
-
 import torch
 
 from e3nn.util._argtools import _get_device
 
 from nequip.data import AtomicDataDict
-
 from ._graph_mixin import GraphModuleMixin
+
+from typing import List, Dict, Any, Optional, Final
+
+
+R_MAX_KEY: Final[str] = "r_max"
+PER_EDGE_TYPE_CUTOFF_KEY: Final[str] = "per_edge_type_cutoff"
+TYPE_NAMES_KEY: Final[str] = "type_names"
+NUM_TYPES_KEY: Final[str] = "num_types"
+MODEL_DTYPE_KEY: Final[str] = "model_dtype"
+
+
+def _model_metadata_from_config(model_config: Dict[str, str]) -> Dict[str, Any]:
+    model_metadata_dict = {}
+    # manually process everything
+    model_metadata_dict[MODEL_DTYPE_KEY] = model_config[MODEL_DTYPE_KEY]
+    model_metadata_dict[TYPE_NAMES_KEY] = " ".join(model_config[TYPE_NAMES_KEY])
+    model_metadata_dict[NUM_TYPES_KEY] = len(model_config[TYPE_NAMES_KEY])
+    model_metadata_dict[R_MAX_KEY] = str(model_config[R_MAX_KEY])
+
+    if PER_EDGE_TYPE_CUTOFF_KEY in model_config:
+        from .embedding._edge import _process_per_edge_type_cutoff
+
+        per_edge_type_cutoff = _process_per_edge_type_cutoff(
+            model_config[TYPE_NAMES_KEY],
+            model_config[PER_EDGE_TYPE_CUTOFF_KEY],
+            model_config[R_MAX_KEY],
+        )
+        model_metadata_dict[PER_EDGE_TYPE_CUTOFF_KEY] = " ".join(
+            str(e.item()) for e in per_edge_type_cutoff.view(-1)
+        )
+    return model_metadata_dict
 
 
 class GraphModel(GraphModuleMixin, torch.nn.Module):
@@ -16,20 +44,15 @@ class GraphModel(GraphModuleMixin, torch.nn.Module):
 
     Args:
         model (GraphModuleMixin): model to wrap
-        type_names (List[str]): model atom type names
-        model_dtype (torch.dtype): dtype of model (``torch.float32`` or ``torch.float64``)
         model_input_fields (Dict[str, Any]): input fields and their irreps
     """
 
-    type_names: List[str]
-    model_dtype: torch.dtype
     model_input_fields: List[str]
 
     def __init__(
         self,
         model: GraphModuleMixin,
-        type_names: List[str],
-        model_dtype: Optional[torch.dtype] = None,
+        model_config: Optional[Dict[str, str]] = None,
         model_input_fields: Dict[str, Any] = {},
     ) -> None:
         super().__init__()
@@ -52,12 +75,19 @@ class GraphModel(GraphModuleMixin, torch.nn.Module):
                     f"Model has `{k}` in its irreps_in with irreps `{irreps}`, but `{k}` is missing from/has inconsistent irreps in model_input_fields of `{self.irreps_in.get(k, 'missing')}`"
                 )
         self.model = model
-        # type names and model_dtype aren't actually used -- they're here for recording purposes
-        self.type_names = type_names
-        self.model_dtype = (
-            model_dtype if model_dtype is not None else torch.get_default_dtype()
-        )
         self.model_input_fields = list(self.irreps_in.keys())
+
+        # the following logic is for backward compatibility and to simplify unittests
+        self.model_dtype = torch.get_default_dtype()
+        self.metadata = {}
+        self.type_names = []
+        if model_config is not None:
+            self.metadata = _model_metadata_from_config(model_config)
+            self.type_names = self.metadata[TYPE_NAMES_KEY]
+            model_dtype = {"float32": torch.float32, "float64": torch.float64}[
+                self.metadata[MODEL_DTYPE_KEY]
+            ]
+            assert self.model_dtype == model_dtype
 
     def forward(self, data: AtomicDataDict.Type) -> AtomicDataDict.Type:
         # restrict the input data to allowed keys to prevent the model from directly using the dict from the outside,
