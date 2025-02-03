7ca20ae431a289e5270ff0a8dc26835f26ca10c2
cw-tan <chuinwei.tan@gmail.com>
2025-02-03 16:46:00 -0500

    remove unused `per_species_set` pytest fixture

--- nequip/utils/unittests/conftest.py
+++ nequip/utils/unittests/conftest.py
@@ -160,22 +160,5 @@ def atomic_batch(nequip_dataset):
     return AtomicDataDict.batched_from_list([nequip_dataset[0], nequip_dataset[1]])
 
 
-@pytest.fixture(scope="function")
-def per_species_set():
-    dtype = torch.get_default_dtype()
-    rng = torch.Generator().manual_seed(127)
-    mean_min = 1
-    mean_max = 100
-    std = 20
-    n_sample = 1000
-    n_species = 9
-    ref_mean = torch.rand((n_species), generator=rng) * (mean_max - mean_min) + mean_min
-    t_mean = torch.ones((n_sample, 1)) * ref_mean.reshape([1, -1])
-    ref_std = torch.rand((n_species), generator=rng) * std
-    t_std = torch.ones((n_sample, 1)) * ref_std.reshape([1, -1])
-    E = torch.normal(t_mean, t_std, generator=rng)
-    return ref_mean.to(dtype), ref_std.to(dtype), E.to(dtype), n_sample, n_species
-
-
 # Use debug mode
 set_irreps_debug(True)
