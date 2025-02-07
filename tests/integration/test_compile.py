fd849062927c033dea75cbe1d6df61d052df8dfe
Sean Kavanagh <s.kavanagh19@imperial.ac.uk>
2025-02-07 10:29:28 -0500

    Update `test_compile` to checked swapped devices

--- tests/integration/test_compile.py
+++ tests/integration/test_compile.py
@@ -16,9 +16,10 @@ import uuid
 from hydra.utils import instantiate
 
 
-@pytest.mark.parametrize(
-    "device", ["cpu"] + (["cuda"] if torch.cuda.is_available() else [])
-)
+available_devices = ["cpu"] + (["cuda"] if torch.cuda.is_available() else [])
+
+
+@pytest.mark.parametrize("device", available_devices)
 @pytest.mark.parametrize(
     "mode", ["torchscript"] + (["aotinductor"] if _TORCH_GE_2_6 else [])
 )
@@ -76,39 +77,52 @@ def test_compile(fake_model_training_session, device, mode):
     ), f"Compiled model `{output_path}` does not exist!"
 
     # == get ase calculator for checkpoint and compiled models ==
-    ckpt_calc = NequIPCalculator.from_checkpoint_model(
-        ckpt_path,
-        device=device,
-        set_global_options=False,
-    )
-    compile_calc = NequIPCalculator.from_compiled_model(
-        output_path,
-        device=device,
-    )
+    for load_device in available_devices:
+        if mode == "aotinductor" and load_device != device:
+            with pytest.raises(
+                RuntimeError
+            ):  # aotinductor does not support loading on different devices
+                NequIPCalculator.from_compiled_model(
+                    output_path,
+                    device=load_device,
+                )
+            continue
 
-    # == get validation data by instantiating datamodules ==
-    datamodule = instantiate(config.data, _recursive_=False)
-    datamodule.prepare_data()
-    datamodule.setup("validate")
-    dloader = datamodule.val_dataloader()[0]
+        # test both devices if possible, where we are testing swapping of devices between compile and
+        # loading (CUDA -> cpu and vice versa)
+        ckpt_calc = NequIPCalculator.from_checkpoint_model(
+            ckpt_path,
+            device=load_device,
+            set_global_options=False,
+        )
+        compile_calc = NequIPCalculator.from_compiled_model(
+            output_path,
+            device=load_device,
+        )
 
-    # == loop over data and do checks ==
-    for data in dloader:
-        atoms_list = to_ase(data.copy())
-        for atoms in atoms_list:
-            ckpt_atoms, compile_atoms = atoms.copy(), atoms.copy()
-            ckpt_atoms.calc = ckpt_calc
-            ckpt_E = ckpt_atoms.get_potential_energy()
-            ckpt_F = ckpt_atoms.get_forces()
+        # == get validation data by instantiating datamodules ==
+        datamodule = instantiate(config.data, _recursive_=False)
+        datamodule.prepare_data()
+        datamodule.setup("validate")
+        dloader = datamodule.val_dataloader()[0]
 
-            compile_atoms.calc = compile_calc
-            compile_E = compile_atoms.get_potential_energy()
-            compile_F = compile_atoms.get_forces()
+        # == loop over data and do checks ==
+        for data in dloader:
+            atoms_list = to_ase(data.copy())
+            for atoms in atoms_list:
+                ckpt_atoms, compile_atoms = atoms.copy(), atoms.copy()
+                ckpt_atoms.calc = ckpt_calc
+                ckpt_E = ckpt_atoms.get_potential_energy()
+                ckpt_F = ckpt_atoms.get_forces()
 
-            del atoms, ckpt_atoms, compile_atoms
-            assert np.allclose(ckpt_E, compile_E, rtol=tol, atol=tol), np.max(
-                np.abs((ckpt_E - compile_E))
-            )
-            assert np.allclose(ckpt_E, compile_E, rtol=tol, atol=tol), np.max(
-                np.abs((ckpt_F - compile_F))
-            )
+                compile_atoms.calc = compile_calc
+                compile_E = compile_atoms.get_potential_energy()
+                compile_F = compile_atoms.get_forces()
+
+                del atoms, ckpt_atoms, compile_atoms
+                assert np.allclose(ckpt_E, compile_E, rtol=tol, atol=tol), np.max(
+                    np.abs((ckpt_E - compile_E))
+                )
+                assert np.allclose(ckpt_E, compile_E, rtol=tol, atol=tol), np.max(
+                    np.abs((ckpt_F - compile_F))
+                )
