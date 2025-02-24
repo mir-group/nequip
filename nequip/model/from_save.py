e07c8ec830af345178fedf0a1a4bb5ec983f9687
cw-tan <chuinwei.tan@gmail.com>
2025-02-23 22:25:00 -0500

    make setting of global state the responsibility of `ModelFromCheckpoint` clients

--- nequip/model/from_save.py
+++ nequip/model/from_save.py
@@ -1,17 +1,11 @@
 import torch
 from nequip.utils import get_current_code_versions
-from nequip.utils._global_options import _set_global_options
 
-from omegaconf import OmegaConf
 import hydra
 import warnings
-from typing import Union
 
 
-def ModelFromCheckpoint(
-    checkpoint_path: str,
-    set_global_options: Union[str, bool] = False,
-):
+def ModelFromCheckpoint(checkpoint_path: str):
     """Builds model from a NequIP framework checkpoint file.
 
     There are two common use modes of this model builder.
@@ -24,7 +18,7 @@ def ModelFromCheckpoint(
       - will ignore the global options from the checkpoint file, and
       - will use the ``compile_mode`` from the checkpoint (unless whoever calls this uses the ``compile_mode`` overriding context manager)
 
-    The script using this model builder is responsible for setting up the global options and potentially overriding the compile mode.
+    The client using this model builder is responsible for setting up the global options and potentially overriding the compile mode.
 
     Args:
         checkpoint_path (str): path to a ``nequip`` framework checkpoint file
@@ -44,16 +38,10 @@ def ModelFromCheckpoint(
         if code in ckpt_versions:
             ckpt_version = ckpt_versions[code]
             # sanity check that versions for current build matches versions from ckpt
-            # TODO: or should we just throw an error
             if ckpt_version != session_version:
                 warnings.warn(
                     f"`{code}` versions differ between the checkpoint file ({ckpt_version}) and the current run ({session_version}) -- `ModelFromCheckpoint` will be built with the current run's versions, but please check that this decision is as intended."
                 )
-
-    if set_global_options:
-        global_options = checkpoint["hyper_parameters"]["info_dict"]["global_options"]
-        global_options.update({"warn_on_override": set_global_options == "warn"})
-        _set_global_options(**global_options)
 
     # === load model via lightning module ===
     training_module = hydra.utils.get_class(
@@ -64,10 +52,7 @@ def ModelFromCheckpoint(
     return model
 
 
-def ModelFromPackage(
-    package_path: str,
-    set_global_options: Union[str, bool] = False,
-):
+def ModelFromPackage(package_path: str):
     """Builds model from a NequIP framework packaged zip file constructed with ``nequip-package``.
 
     Args:
@@ -85,16 +70,6 @@ def ModelFromPackage(
             module="torch.package.package_importer",
         )
         imp = torch.package.PackageImporter(package_path)
-        # set global options before loading model
-        # TODO: do we need to set global options with the packaged code?
-        # for now, nothing in global options should require this, but maybe if e3nn were interned ...
-        if set_global_options:
-            config = imp.load_text(package="model", resource="config.yaml")
-            global_options = OmegaConf.to_container(OmegaConf.create(config))[
-                "global_options"
-            ]
-            global_options.update({"warn_on_override": set_global_options == "warn"})
-            _set_global_options(**global_options)
         model = imp.load_pickle(package="model", resource="model.pkl")
 
     # NOTE: model returned is not a GraphModel object tied to the `nequip` in current Python env, but a GraphModel object from the packaged zip file
