cb2811b69d35b4c57b5396c7d42c2190c93cef16
cw-tan <chuinwei.tan@gmail.com>
2025-03-06 23:52:35 -0500

    allow configurable floating point tolerances through env vars

--- nequip/utils/dtype.py
+++ nequip/utils/dtype.py
@@ -1,7 +1,7 @@
-from typing import Union
-import contextlib
-
 import torch
+import os
+import contextlib
+from typing import Union, Final
 
 
 def dtype_from_name(name: Union[str, torch.dtype]) -> torch.dtype:
@@ -30,10 +30,14 @@ def torch_default_dtype(dtype):
         torch.set_default_dtype(orig_default_dtype)
 
 
-_FLOAT64_MODEL_TOL = 1e-12
-_FLOAT32_MODEL_TOL = 5e-5
-_TF32_MODEL_TOL = 1e-4
-# TODO: ^ TF32 tolerance is untested
+# === floating point tolerances as env vars ===
+_FLOAT64_MODEL_TOL: Final[float] = float(
+    os.environ.get("NEQUIP_FLOAT64_MODEL_TOL", 1e-12)
+)
+_FLOAT32_MODEL_TOL: Final[float] = float(
+    os.environ.get("NEQUIP_FLOAT32_MODEL_TOL", 5e-5)
+)
+_TF32_MODEL_TOL: Final[float] = float(os.environ.get("NEQUIP_TF32_MODEL_TOL", 1e-4))
 
 
 def floating_point_tolerance(model_dtype: Union[str, torch.dtype]):
