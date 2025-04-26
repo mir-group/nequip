be3860653fccfcc8d7523920ba2bb676ac763c6d
cw-tan <chuinwei.tan@gmail.com>
2025-04-25 23:57:35 -0400

    simplify ZBL tests

--- tests/unit/model/test_pair/test_zbl.py
+++ tests/unit/model/test_pair/test_zbl.py
@@ -39,31 +39,34 @@ class TestZBLModel(BaseEnergyModelTests):
 
     def test_lammps_repro(self, model, device):
         ZBL_model, config, _ = model
-        if config["model_dtype"] != "float64":
-            pytest.skip()
-        transforms = [
-            ChemicalSpeciesToAtomTypeMapper(
-                chemical_symbols=config["chemical_species"],
-            ),
-            NeighborListTransform(r_max=_ZBL_TEST_RMAX),
-        ]
-        ZBL_model.eval()
-        # make test system of two atoms:
-        atoms = ase.Atoms(positions=np.zeros((2, 3)), symbols=["H", "H"])
-        atoms.calc = NequIPCalculator(ZBL_model, device=device, transforms=transforms)
-        # == load precomputed reference data ==
-        # To regenerate this data, run
-        # $ lmp -in zbl_data.lmps
-        # $ python -c "import numpy as np; d = np.loadtxt('zbl.dat', skiprows=1); np.save('zbl.npy', d)"
-        refdata = np.load(Path(__file__).parent / "zbl.npy")
-        for r, Zi, Zj, pe, fxi, fxj in refdata:
-            if r >= _ZBL_TEST_RMAX:
-                continue
-            atoms.positions[1, 0] = r
-            atoms.set_atomic_numbers([int(Zi), int(Zj)])
-            # ZBL blows up for atoms being close, so the numerics differ to ours
-            # 1e-5 == 0.01 meV / Å
-            assert np.allclose(atoms.get_forces()[0, 0], fxi, atol=1e-5)
-            assert np.allclose(atoms.get_forces()[1, 0], fxj, atol=1e-5)
-            # 1e-4 == 0.1 meV system, 0.05 meV / atom
-            assert np.allclose(atoms.get_potential_energy(), pe, atol=1e-4)
+
+        # run test only if model is float64
+        if config["model_dtype"] == "float64":
+            transforms = [
+                ChemicalSpeciesToAtomTypeMapper(
+                    chemical_symbols=config["chemical_species"],
+                ),
+                NeighborListTransform(r_max=_ZBL_TEST_RMAX),
+            ]
+            ZBL_model.eval()
+            # make test system of two atoms:
+            atoms = ase.Atoms(positions=np.zeros((2, 3)), symbols=["H", "H"])
+            atoms.calc = NequIPCalculator(
+                ZBL_model, device=device, transforms=transforms
+            )
+            # == load precomputed reference data ==
+            # To regenerate this data, run
+            # $ lmp -in zbl_data.lmps
+            # $ python -c "import numpy as np; d = np.loadtxt('zbl.dat', skiprows=1); np.save('zbl.npy', d)"
+            refdata = np.load(Path(__file__).parent / "zbl.npy")
+            for r, Zi, Zj, pe, fxi, fxj in refdata:
+                if r >= _ZBL_TEST_RMAX:
+                    continue
+                atoms.positions[1, 0] = r
+                atoms.set_atomic_numbers([int(Zi), int(Zj)])
+                # ZBL blows up for atoms being close, so the numerics differ to ours
+                # 1e-5 == 0.01 meV / Å
+                assert np.allclose(atoms.get_forces()[0, 0], fxi, atol=1e-5)
+                assert np.allclose(atoms.get_forces()[1, 0], fxj, atol=1e-5)
+                # 1e-4 == 0.1 meV system, 0.05 meV / atom
+                assert np.allclose(atoms.get_potential_energy(), pe, atol=1e-4)
