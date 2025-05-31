853f167ca0ecd7cf9f138f24ec6ea43f7d91cdcf
cw-tan <chuinwei.tan@gmail.com>
2025-05-31 01:23:42 -0400

    introduce `SortedNeighborListTransform` with tests

--- nequip/data/transforms/__init__.py
+++ nequip/data/transforms/__init__.py
@@ -1,11 +1,12 @@
 # This file is a part of the `nequip` package. Please see LICENSE and README at the root for information on using it.
 from .type_mapper import ChemicalSpeciesToAtomTypeMapper
-from .neighborlist import NeighborListTransform
+from .neighborlist import NeighborListTransform, SortedNeighborListTransform
 from .stress_utils import VirialToStressTransform, StressSignFlipTransform
 
 __all__ = [
     "ChemicalSpeciesToAtomTypeMapper",
     "NeighborListTransform",
+    "SortedNeighborListTransform",
     "VirialToStressTransform",
     "StressSignFlipTransform",
 ]
