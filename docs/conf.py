33f972a10f0765c4faeceecc60a104954802f578
cw-tan <chuinwei.tan@gmail.com>
2025-07-18 16:43:11 -0400

    justify docs

--- docs/conf.py
+++ docs/conf.py
@@ -75,6 +75,8 @@ html_theme = "furo"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ["_static"]
+html_css_files = ["custom.css"]
 html_favicon = "favicon.png"
 html_logo = "logo.png"
 html_theme_options = {
