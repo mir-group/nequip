d6a98203a4da0c8bd5723e9ba4082adfa6172c44
cw-tan <chuinwei.tan@gmail.com>
2026-02-21 17:15:47 -0500

    clean datamodule init

--- nequip/data/datamodule/__init__.py
+++ nequip/data/datamodule/__init__.py
@@ -1,8 +1,8 @@
 # This file is a part of the `nequip` package. Please see LICENSE and README at the root for information on using it.
 from ._base_datamodule import NequIPDataModule
+from ._ase_datamodule import ASEDataModule
 from .sgdml_datamodule import sGDML_CCSD_DataModule
 from ._3bpa_datamodule import NequIP3BPADataModule
-from ._ase_datamodule import ASEDataModule
 from .tm23_datamodule import TM23DataModule
 from .rmd17_datamodule import rMD17DataModule
 from .md22_datamodule import MD22DataModule
@@ -11,14 +11,14 @@ from .water_datamodule import WaterDataModule
 from .coll_datamodule import COLLDataModule
 
 __all__ = [
-    NequIPDataModule,
-    sGDML_CCSD_DataModule,
-    rMD17DataModule,
-    MD22DataModule,
-    NequIP3BPADataModule,
-    TM23DataModule,
-    ASEDataModule,
-    SAMD23DataModule,
-    WaterDataModule,
-    COLLDataModule,
+    "NequIPDataModule",
+    "ASEDataModule",
+    "sGDML_CCSD_DataModule",
+    "rMD17DataModule",
+    "MD22DataModule",
+    "NequIP3BPADataModule",
+    "TM23DataModule",
+    "SAMD23DataModule",
+    "WaterDataModule",
+    "COLLDataModule",
 ]
